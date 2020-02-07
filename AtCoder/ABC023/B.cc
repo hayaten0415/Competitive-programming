14e3b190ecbcad37b5861016e421e63a7e57c09c
Hayashi Hironori <hayaten415@gmail.com>
2020-02-07 23:24:35 +0900

    ABC023 B　rewrite

--- AtCoder/ABC023/B.cc
+++ AtCoder/ABC023/B.cc
@@ -2,94 +2,17 @@
 #include <algorithm>
 using namespace std;
 
-
-int remainder1mod3(int n, string s) {
-  for (int i = 0; i < n; i++){
-    if (i % 3 == 0){
-      if (s.at(i) == 'b'){
-        continue;
-      }else {
-        return -1;
-      }
-    } else if(i % 3 == 1) {
-      if(s.at(i) == 'c') {
-        continue;
-      } else {
-        return -1;
-      }
-    } else {
-      if(s.at(i) == 'a') {
-        continue;
-      } else {
-        return -1;
-      }
-    }
-  }
-  return n / 2;
-}
-
-int remainder2mod3(int n, string s) {
-  for (int i = 0; i < n; i++)
-  {
-    if (i % 3 == 0){
-      if (s.at(i) == 'c'){
-        continue;
-      }else {
-        return -1;
-      }
-    } else if(i % 3 == 1) {
-      if(s.at(i) == 'a') {
-        continue;
-      } else {
-        return -1;
-      }
-    } else {
-      if(s.at(i) == 'b') {
-        continue;
-      } else {
-        return -1;
-      }
-    }
-  }
-  return n / 2;
-}
-
-int remainder0mod3(int n, string s) {
-  for (int i = 0; i < n; i++){
-    if (i % 3 == 0){
-      if (s.at(i) == 'a'){
-        continue;
-      }else {
-        return -1;
-      }
-    } else if(i % 3 == 1) {
-      if(s.at(i) == 'b') {
-        continue;
-      } else {
-        return -1;
-      }
-    } else {
-      if(s.at(i) == 'c') {
-        continue;
-      } else {
-        return -1;
-      }
-    }
-  }
-  return n / 2;
-}
-
-
-
 int main()
 {
-  int n, ans;
-  string s;
-  cin >> n >> s;
-  if(n % 3 == 1) ans = remainder1mod3(n, s);
-  if(n % 3 == 2) ans = remainder2mod3(n, s);
-  if(n % 3 == 0) ans = remainder0mod3(n, s);
-  if (n % 2 == 0) ans = -1;
-  cout << ans << endl;
+  int n, i;
+  string S, s="b";
+  cin >> n >> S;
+  for (i = 0; i <= n; i++){
+    if(s.size()>=n) break;
+    if(i % 3 == 0) s = 'a' + s +'c';
+    if(i % 3 == 1) s = 'c' + s +'a';
+    if(i % 3 == 2) s = 'b' + s + 'b';
+  }
+  cout << (s == S ? i : -1) << endl;
   return 0;
 }
