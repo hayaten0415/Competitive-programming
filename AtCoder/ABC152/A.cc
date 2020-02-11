2a4f8d922408a096e783a02c770d3a358aa2f9a5
Hayashi Hironori <hayaten415@gmail.com>
2020-02-11 15:32:08 +0900

    ABC152 A,B,C,D

---
+++ AtCoder/ABC152/A.cc
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+#define rep(i, n) for (int i = 0; i < (n); i++)
+using namespace std;
+using ll = long long;
+int main() {
+  int n, m;
+  cin >> n >> m;
+  cout << (n == m ? "Yes" : "No") << endl;
+  return 0;
+}
