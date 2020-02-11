4f4afa07174a0e7c9c22bd0158bef315aa425109
Hayashi Hironori <hayaten415@gmail.com>
2020-02-11 17:40:04 +0900

    template.cc add gcd isPrime

--- AtCoder/template.cc
+++ AtCoder/template.cc
@@ -2,6 +2,33 @@
 #define rep(i, n) for (int i = 0; i < (n); i++)
 using namespace std;
 using ll = long long;
+
+//ユーグリッドの互除法による最大公約数を求めるメソッド
+int gcd(int a, int b) {
+  int smaller = min(a, b);
+  int bigger = max(a, b);
+  int surplus = bigger % smaller;
+  if (surplus == 0) return smaller;
+  surplus = gcd(smaller, surplus);
+  return surplus;
+}
+
+//素数判定
+bool isPrime(int num) {
+    if (num < 2) return false;
+    else if (num == 2) return true;
+    else if (num % 2 == 0) return false; // 偶数はあらかじめ除く
+    double sqrtNum = sqrt(num);
+    for (int i = 3; i <= sqrtNum; i += 2) {
+      if (num % i == 0) {
+        // 素数ではない
+        return false;
+      }
+    }
+    // 素数である
+    return true;
+  }
+
 int main() {
   return 0;
 }
