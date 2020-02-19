0a9e897a12b8c148560b46775cf355aea94c18da
Hayashi Hironori <hayaten415@gmail.com>
2020-02-19 22:29:33 +0900

    累積和で解き直した

--- AtCoder/ABC154/D.cc
+++ AtCoder/ABC154/D.cc
@@ -6,20 +6,16 @@ int main() {
   int n, k;
   cin >> n >> k;
   vector<int> p(n);
-  rep(i, n) cin >> p[i];
-  rep(i, n) p[i]++;
+  vector<int> sum(n + 1);
+  sum[0] = 0;
+  rep(i, n) {
+    cin >> p[i];
+    p[i]++;
+    sum[i + 1] = sum[i] + p[i];
+  }
   int mx = 0;
-  int s = 0;
-  queue<int> q;
-  rep(i, n){
-    s += p[i];
-    q.push(p[i]);
-    if(q.size() > k) {
-      s -= q.front(); q.pop();
-    }
-    if(q.size() == k) {
-      mx = max(mx, s);
-    }
+  for (int i = 0; i + k < n + 1; i++){
+    mx = max(mx, sum[i + k]- sum[i]);
   }
   double ans = mx;
   ans /= 2;
