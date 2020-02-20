47ee9727bf7bd09aff854e74d95df4dcef50be3e
Hayashi Hironori <hayaten415@gmail.com>
2020-02-20 21:43:57 +0900

    longlongに変更

--- AtCoder/template.cc
+++ AtCoder/template.cc
@@ -4,17 +4,17 @@ using namespace std;
 using ll = long long;
 
 //ユーグリッドの互除法による最大公約数を求めるメソッド
-long gcd(long a, long b) {
-  long smaller = min(a, b);
-  long bigger = max(a, b);
-  long surplus = bigger % smaller;
+ll gcd(ll a, ll b) {
+  ll smaller = min(a, b);
+  ll bigger = max(a, b);
+  ll surplus = bigger % smaller;
   if (surplus == 0) return smaller;
   surplus = gcd(smaller, surplus);
   return surplus;
 }
 
 //最小公倍数を求めるメソッド
-long lcm(long a, long b) {
+ll lcm(ll a, ll b) {
   return a * b / gcd(a, b);
 }
 
