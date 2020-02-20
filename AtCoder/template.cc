056ec223161949d98c07d9c105cbcfee04c8e2e9
Hayashi Hironori <hayaten415@gmail.com>
2020-02-20 21:40:47 +0900

    long型に変更、そして最小公倍数を求めるメソッド追加

--- AtCoder/template.cc
+++ AtCoder/template.cc
@@ -4,13 +4,18 @@ using namespace std;
 using ll = long long;
 
 //ユーグリッドの互除法による最大公約数を求めるメソッド
-int gcd(int a, int b) {
-  int smaller = min(a, b);
-  int bigger = max(a, b);
-  int surplus = bigger % smaller;
+long gcd(long a, long b) {
+  long smaller = min(a, b);
+  long bigger = max(a, b);
+  long surplus = bigger % smaller;
   if (surplus == 0) return smaller;
   surplus = gcd(smaller, surplus);
   return surplus;
+}
+
+//最小公倍数を求めるメソッド
+long lcm(long a, long b) {
+  return a * b / gcd(a, b);
 }
 
 //素数判定
@@ -27,7 +32,7 @@ bool isPrime(int num) {
     }
     // 素数である
     return true;
-  }
+}
 
 int main() {
   return 0;
