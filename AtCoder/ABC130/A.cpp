8dd7d069bdd4f963c22dd6962a4329c6fb7ed688
Hayashi Hironori <hayaten415@gmail.com>
2020-03-17 23:16:38 +0900

    ABC130 A

---
+++ AtCoder/ABC130/A.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+#define rep(i, n) for (int i = 0; i < (n); i++)
+using namespace std;
+using ll = long long;
+
+int main(){
+  int a, x;
+  cin >> x >> a;
+  cout << (x < a ? 0 : 10) << endl;
+}
