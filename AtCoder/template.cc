20ab17a8268e89f8b9da1dfa982648507b5f1080
Hayashi Hironori <hayaten415@gmail.com>
2020-04-12 12:53:13 +0900

    template modified

--- AtCoder/template.cc
+++ AtCoder/template.cc
@@ -1,7 +1,7 @@
 #include <bits/stdc++.h>
 #define rep(i, n) for (int i = 0; i < (n); i++)
 using namespace std;
-using ll = long long;
+typedef long long ll;
 
 const int MOD = 1000000007;
 const int MAX =200000;
