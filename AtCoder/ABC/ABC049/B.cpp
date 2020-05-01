8064915219f4da586de406456a3ded5aed5c35a6
hayaten0415 <hayaten415@gmail.com>
2020-05-01 16:09:12 +0900

    ABC049 A,B

---
+++ AtCoder/ABC/ABC049/B.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+#define rep(i, n) for (int i = 0; i < (n); i++)
+using namespace std;
+using P = pair<int, int>;
+typedef long long ll;
+
+int main() {
+  int h, w;
+	cin >> h >> w;
+	vector<string> c(h);
+	rep(i,h){
+		cin >> c[i];
+		cout << c[i] << endl;
+		cout << c[i] << endl;
+	}
+	return 0;
+}
