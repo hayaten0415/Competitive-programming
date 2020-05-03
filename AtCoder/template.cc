1e6a6aa145ef6059d1fde8f7822145a5703cea90
Hayashi Hironori <hayaten415@gmail.com>
2020-05-03 16:04:10 +0900

    4方向ベクトル

--- AtCoder/template.cc
+++ AtCoder/template.cc
@@ -4,6 +4,9 @@ using namespace std;
 using P = pair<int, int>;
 typedef long long ll;
 
+const int dx[4] = {1, 0, -1, 0};
+const int dy[4] = {0, 1, 0, -1};
+
 const int MOD = 1000000007;
 const int MAX =200000;
 
