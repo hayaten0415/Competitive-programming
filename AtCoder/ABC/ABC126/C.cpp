eb08c75480b256be6eb832111ad5936e0493681e
Hayashi Hironori <hayaten415@gmail.com>
2020-05-17 16:21:30 +0900

    ABC126 C

--- AtCoder/ABC/ABC126/C.cpp
+++ AtCoder/ABC/ABC126/C.cpp
@@ -3,22 +3,31 @@
 using namespace std;
 typedef long long ll;
 
-
-ll RepeatSquaring(ll N, ll P){
-    if(P==0) return 1;
-    if(P%2==0){
-        ll t = RepeatSquaring(N, P/2);
-        return t*t;
-    }
-    return N * RepeatSquaring(N, P-1);
+//繰り返し自乗法(modなし)
+int mypow(int x, int n){
+  if(n == 0)
+    return 1;
+ 
+  if(n % 2 == 0)
+    return mypow(x * x, n / 2);
+  else
+    return x * mypow(x, n - 1);
 }
 
 int main() {
   int n, k;
   cin >> n >> k;
   double ans = 0;
+  double coin = 0.5;
   for (int i = 1; i <= n; i++){
-    double per = (1 / n);
-    ()
+    double per = (double)1 / (double)n;
+    int v = i;
+    int c = 0;
+    while(v < k){
+      v *= 2;
+      c++;
+    }
+    ans += per * (double)pow(coin, c);
   }
+  cout << fixed << setprecision(12) << ans << endl;
 }
