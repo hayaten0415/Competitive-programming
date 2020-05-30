c71e1c62512d35bb39f411102fdba308a35b046c
Hayashi Hironori <hayaten415@gmail.com>
2020-05-30 18:28:32 +0900

    ABC114 A

---
+++ AtCoder/ABC/ABC114/A.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+#define rep(i, n) for (int i = 0; i < (n); i++)
+using namespace std;
+using P = pair<int, int>;
+typedef long long ll;
+
+int main() {
+  int x;
+  cin >> x;
+  if(x == 3 || x == 5 || x == 7){
+    cout << "YES" << endl;
+  }else{
+    cout << "NO" << endl;
+  }
+}
