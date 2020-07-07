fe712b8db9341e5915d74b96e2908e267da4bdfb
Hayashi Hironori <hayaten415@gmail.com>
2020-07-07 21:16:27 +0900

    ABC173 A

---
+++ AtCoder/ABC/ABC107/A.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+#define rep(i, n) for (int i = 0; i < (n); i++)
+using namespace std;
+using P = pair<int, int>;
+typedef long long ll;
+
+int main() {
+  int n, i;
+  cin >> n >> i;
+  cout << n - i + 1 << endl;
+}
