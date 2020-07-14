0d3afbaf412a28f6c0c4d7f458ae48900e2fd808
Hayashi Hironori <hayaten415@gmail.com>
2020-07-14 23:34:59 +0900

    DSL3_C

---
+++ AOJ/DSL/3/C.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+#define rep(i, n) for (int i = 0; i < (n); i++)
+using namespace std;
+using P = pair<int, int>;
+typedef long long ll;
+
+
+
+int main() {
+  int n, q;
+  cin >> n >> q;
+  vector<ll> a(n);
+  rep(i, n){
+    cin >> a[i];
+  }
+  rep(i,q){
+    ll x;
+    cin >> x;
+    ll ans = 0;
+    ll sum = 0;
+    int right = 0;
+    for (int left = 0; left < n; left++){
+      while(right < n &&sum + a[right] <= x){
+        sum += a[right];
+        right++;
+      }
+      ans += right - left;
+      if(right == left)right++;
+      else sum -= a[left];
+    }
+    cout << ans << endl;
+  }
+}
