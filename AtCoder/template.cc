39a694ab585c70b3c7bc5b3150163342833d18c2
Hayashi Hironori <hayaten415@gmail.com>
2020-07-15 20:53:17 +0900

    syakutori

--- AtCoder/template.cc
+++ AtCoder/template.cc
@@ -10,6 +10,23 @@ const int dy[4] = {0, 1, 0, -1};
 const int MOD = 1000000007;
 const int MAX =200000;
 
+void syakutori{
+  int right = 0;     
+  for (int left = 0; left < n; ++left) {
+      while (right < n && (right を 1 個進めたときに条件を満たす)) {
+          /* 実際に right を 1 進める */
+          // ex: sum += a[right];
+          ++right;
+      }
+      /* break した状態で right は条件を満たす最大なので、何かする */
+      // ex: res += (right - left);
+
+      /* left をインクリメントする準備 */
+      // ex: if (right == left) ++right;
+      // ex: else sum -= a[left];
+  }
+}
+
 //素因数分解
 vector<pair<ll,ll>> prime_factorize(ll n) {
   vector<pair<ll, ll>> res;
