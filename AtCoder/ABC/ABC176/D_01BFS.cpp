997d4bc618226c402e53d8c5a2a98752b87d06d1
Hayashi Hironori <hayaten415@gmail.com>
2020-08-23 19:53:29 +0900

    ABC176 D 01BFS

---
+++ AtCoder/ABC/ABC176/D_01BFS.cpp
@@ -0,0 +1,59 @@
+#include <bits/stdc++.h>
+#define rep(i, n) for (int i = 0; i < (n); i++)
+#define rrep(i, n) for (int i = (n - 1); i >= 0; i--)
+#define ALL(v) v.begin(), v.end()
+using namespace std;
+using P = pair<int, int>;
+typedef long long ll;
+template<class T> inline bool chmax(T& a, T b) { if (a < b) { a = b; return 1; } return 0; }
+template<class T> inline bool chmin(T& a, T b) { if (a > b) { a = b; return 1; } return 0; }
+const int dx[4] = {1, 0, -1, 0};
+const int dy[4] = {0, 1, 0, -1};
+int dist[1000][1000];
+string grid[1000];
+int h, w, sy, sx, gy, gx;
+const int INF = (1 << 30);
+
+int main() {
+  cin >> h >> w;
+  cin >> sy >> sx;
+  sy--, sx--;
+  cin >> gy >> gx;
+  gy--; gx--;
+  rep(i, h){
+    rep(j, w){
+      dist[i][j] = INF;
+    }
+  }
+  rep(i, h) cin >> grid[i];
+  dist[sy][sx] = 0;
+  deque<P> que;
+  que.push_front(P(sy, sx));
+  while (!que.empty()){
+    P p = que.front();
+    que.pop_front();
+    int cy = p.first, cx = p.second;
+    for (int i = 0; i < 4; i++){
+      int ny = cy + dy[i], nx = cx + dx[i];
+      if (ny < 0 || h <= ny || nx < 0 || w <= nx)continue;
+      if(grid[ny][nx] == '#')continue;
+      if(dist[ny][nx] <= dist[cy][cx])continue;
+      dist[ny][nx] = dist[cy][cx];
+      que.push_front(P(ny, nx));
+    }
+    for (int i = cy -2; i <= cy + 2; i++){
+      for (int j = cx - 2; j <= cx + 2; j++){
+        if (i < 0 || h <= i || j < 0 || w <= j)continue;
+        if(grid[i][j] == '#')continue;
+        if(dist[i][j] <= dist[cy][cx] + 1)continue;
+        dist[i][j] = dist[cy][cx] + 1;
+        que.push_back(P(i, j));
+      }
+    }
+  }
+  if(dist[gy][gx] ==INF){
+    cout << -1 << endl;
+  }else{
+    cout << dist[gy][gx] << endl;
+  }
+}
