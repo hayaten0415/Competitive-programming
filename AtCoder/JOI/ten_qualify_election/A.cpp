bfba105f51c9e4e5ad87d928eba4936306eb1dbe
Hayashi Hironori <hayaten415@gmail.com>
2020-08-25 22:32:05 +0900

    JOI ten qualify election A

---
+++ AtCoder/JOI/ten_qualify_election/A.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+#define rep(i, n) for (int i = 0; i < (n); i++)
+#define rrep(i, n) for (int i = (n - 1); i >= 0; i--)
+#define ALL(v) v.begin(), v.end()
+using namespace std;
+using P = pair<int, int>;
+typedef long long ll;
+template<class T> inline bool chmax(T& a, T b) { if (a < b) { a = b; return 1; } return 0; }
+template<class T> inline bool chmin(T& a, T b) { if (a > b) { a = b; return 1; } return 0; }
+const int dx[4] = {1, 0, -1, 0};
+const int dy[4] = {0, 1, 0, -1};
+
+int main() {
+  int minutes, seconds;
+  int sum = 0;
+  rep(i, 4){
+    int num;
+    cin >> num;
+    sum += num;
+  }
+  minutes = sum / 60;
+  sum -= 60 * minutes;
+  seconds = sum;
+  cout << minutes << endl;
+  cout << seconds << endl;
+}
