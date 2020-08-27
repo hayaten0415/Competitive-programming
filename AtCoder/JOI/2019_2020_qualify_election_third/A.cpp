cbbfa495c49926eef2f94b9a0eec2533d9ed1f83
Hayashi Hironori <hayaten415@gmail.com>
2020-08-27 23:22:24 +0900

    JOI 2019/2020 qualify election A

---
+++ AtCoder/JOI/2019_2020_qualify_election_third/A.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+#define rep(i, n) for (int i = 0; i < (n); i++)
+#define rrep(i, n) for (int i = (n - 1); i >= 0; i--)
+#define ALL(v) v.begin(), v.end()
+using namespace std;
+using P = pair<int, int>;
+typedef long long ll;
+template<class T> inline bool chmax(T& a, T b) { if (a < b) { a = b; return 1; } return 0; }
+template<class T> inline bool chmin(T& a, T b) { if (a > b) { a = b; return 1; } return 0; }
+const int dx[4] = {1, 0, -1, 0};
+const int dy[4] = {0, 1, 0, -1};
+
+int main() {
+  int x, l, r;
+  cin >> x >> l >> r;
+  int mn = (1 << 30);
+  for (int i = l; i <= r; i++){
+    mn = min(mn, abs(x - i));
+  }
+  if(x + mn<= r)cout << x + mn << endl;
+  else cout << x - mn << endl;
+}
