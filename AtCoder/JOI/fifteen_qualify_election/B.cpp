542132f1e617a98d44891438f58b6f6cef6c2c52
Hayashi Hironori <hayaten415@gmail.com>
2020-08-27 16:39:19 +0900

    JOI fifteen qualify election B

---
+++ AtCoder/JOI/fifteen_qualify_election/B.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+#define rep(i, n) for (int i = 0; i < (n); i++)
+#define rrep(i, n) for (int i = (n - 1); i >= 0; i--)
+#define ALL(v) v.begin(), v.end()
+using namespace std;
+using P = pair<int, int>;
+typedef long long ll;
+template<class T> inline bool chmax(T& a, T b) { if (a < b) { a = b; return 1; } return 0; }
+template<class T> inline bool chmin(T& a, T b) { if (a > b) { a = b; return 1; } return 0; }
+const int dx[4] = {1, 0, -1, 0};
+const int dy[4] = {0, 1, 0, -1};
+
+int main() {
+  int n, m;
+  cin >> n >> m;
+  vector<int> A(n);
+  //first number
+  rep(i, n) cin >> A[i];
+  rep(i, m){
+    for(int j = 0; j < n-1; j++){
+      if((A[j] % (i + 1)) > (A[j + 1] % (i + 1)))swap(A[j], A[j+1]);
+    }
+  }
+  rep(i, n)cout << A[i] << "\n";
+}
