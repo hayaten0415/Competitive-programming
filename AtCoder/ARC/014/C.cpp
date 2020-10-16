b91c7f10cd80bed64556d56946d9b0328761b397
Hayashi Hironori <hayaten415@gmail.com>
2020-10-16 22:03:31 +0900

    ARC014 C

---
+++ AtCoder/ARC/014/C.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+#define rep(i, n) for (int i = 0; i < (n); i++)
+#define rrep(i, n) for (int i = (n - 1); i >= 0; i--)
+#define ALL(v) v.begin(), v.end()
+using namespace std;
+using P = pair<int, int>;
+typedef long long ll;
+template<class T> inline bool chmax(T& a, T b) { if (a < b) { a = b; return 1; } return 0; }
+template<class T> inline bool chmin(T& a, T b) { if (a > b) { a = b; return 1; } return 0; }
+const int dx[4] = {1, 0, -1, 0};
+const int dy[4] = {0, 1, 0, -1};
+
+int main() {
+  int n;
+  cin >> n;
+  string s;
+  cin >> s;
+  int r = 0, g = 0, b = 0;
+  rep(i, n){
+    if(s[i] == 'R')r++;
+    if(s[i] == 'G')g++;
+    if(s[i] == 'B')b++;
+  }
+  int ans = 0;
+  ans += r % 2;
+  ans += g % 2;
+  ans += b % 2;
+  cout << ans << endl;
+}
