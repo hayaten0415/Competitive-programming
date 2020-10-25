f4872fd63bfb49ca6117a2b81af0ed022aab124f
Hayashi Hironori <hayaten415@gmail.com>
2020-10-25 15:45:18 +0900

    ARC106 D

---
+++ AtCoder/ARC/106/D.cpp
@@ -0,0 +1,143 @@
+#include <bits/stdc++.h>
+#define rep(i, n) for (int i = 0; i < (n); i++)
+#define rrep(i, n) for (int i = (n - 1); i >= 0; i--)
+#define ALL(v) v.begin(), v.end()
+using namespace std;
+using P = pair<int, int>;
+typedef long long ll;
+template<class T> inline bool chmax(T& a, T b) { if (a < b) { a = b; return 1; } return 0; }
+template<class T> inline bool chmin(T& a, T b) { if (a > b) { a = b; return 1; } return 0; }
+const int dx[4] = {1, 0, -1, 0};
+const int dy[4] = {0, 1, 0, -1};
+
+template<int MOD> struct Fp {
+    long long val;
+    constexpr Fp(long long v = 0) noexcept : val(v % MOD) {
+        if (val < 0) val += MOD;
+    }
+    constexpr int getmod() const { return MOD; }
+    constexpr Fp operator - () const noexcept {
+        return val ? MOD - val : 0;
+    }
+    constexpr Fp operator + (const Fp& r) const noexcept { return Fp(*this) += r; }
+    constexpr Fp operator - (const Fp& r) const noexcept { return Fp(*this) -= r; }
+    constexpr Fp operator * (const Fp& r) const noexcept { return Fp(*this) *= r; }
+    constexpr Fp operator / (const Fp& r) const noexcept { return Fp(*this) /= r; }
+    constexpr Fp& operator += (const Fp& r) noexcept {
+        val += r.val;
+        if (val >= MOD) val -= MOD;
+        return *this;
+    }
+    constexpr Fp& operator -= (const Fp& r) noexcept {
+        val -= r.val;
+        if (val < 0) val += MOD;
+        return *this;
+    }
+    constexpr Fp& operator *= (const Fp& r) noexcept {
+        val = val * r.val % MOD;
+        return *this;
+    }
+    constexpr Fp& operator /= (const Fp& r) noexcept {
+        long long a = r.val, b = MOD, u = 1, v = 0;
+        while (b) {
+            long long t = a / b;
+            a -= t * b, swap(a, b);
+            u -= t * v, swap(u, v);
+        }
+        val = val * u % MOD;
+        if (val < 0) val += MOD;
+        return *this;
+    }
+    constexpr bool operator == (const Fp& r) const noexcept {
+        return this->val == r.val;
+    }
+    constexpr bool operator != (const Fp& r) const noexcept {
+        return this->val != r.val;
+    }
+    friend constexpr istream& operator >> (istream& is, Fp<MOD>& x) noexcept {
+        is >> x.val;
+        x.val %= MOD;
+        if (x.val < 0) x.val += MOD;
+        return is;
+    }
+    friend constexpr ostream& operator << (ostream& os, const Fp<MOD>& x) noexcept {
+        return os << x.val;
+    }
+    friend constexpr Fp<MOD> modpow(const Fp<MOD>& r, long long n) noexcept {
+        if (n == 0) return 1;
+        if (n < 0) return modpow(modinv(r), -n);
+        auto t = modpow(r, n / 2);
+        t = t * t;
+        if (n & 1) t = t * r;
+        return t;
+    }
+    friend constexpr Fp<MOD> modinv(const Fp<MOD>& r) noexcept {
+        long long a = r.val, b = MOD, u = 1, v = 0;
+        while (b) {
+            long long t = a / b;
+            a -= t * b, swap(a, b);
+            u -= t * v, swap(u, v);
+        }
+        return Fp<MOD>(u);
+    }
+};
+
+// Binomial Coefficient
+template<class T> struct BiCoef {
+    vector<T> fact_, inv_, finv_;
+    constexpr BiCoef() {}
+    constexpr BiCoef(int n) noexcept : fact_(n, 1), inv_(n, 1), finv_(n, 1) {
+        init(n);
+    }
+    constexpr void init(int n) noexcept {
+        fact_.assign(n, 1), inv_.assign(n, 1), finv_.assign(n, 1);
+        int MOD = fact_[0].getmod();
+        for(int i = 2; i < n; i++){
+            fact_[i] = fact_[i-1] * i;
+            inv_[i] = -inv_[MOD%i] * (MOD/i);
+            finv_[i] = finv_[i-1] * inv_[i];
+        }
+    }
+    constexpr T com(int n, int k) const noexcept {
+        if (n < k || n < 0 || k < 0) return 0;
+        return fact_[n] * finv_[k] * finv_[n-k];
+    }
+    constexpr T fact(int n) const noexcept {
+        if (n < 0) return 0;
+        return fact_[n];
+    }
+    constexpr T inv(int n) const noexcept {
+        if (n < 0) return 0;
+        return inv_[n];
+    }
+    constexpr T finv(int n) const noexcept {
+        if (n < 0) return 0;
+        return finv_[n];
+    }
+};
+const int MOD = 998244353;
+using mint = Fp<MOD>;
+
+int main() {
+  int N, K;
+  cin >> N >> K;
+  BiCoef<mint> bc(K + 1);
+  vector<mint> A(N);
+  rep(i, N) cin >> A[i];
+  vector<mint> S(K + 1, 0), powA(N, 1);
+  for (int k = 0; k <= K; k++) {
+    for (int i = 0; i < N; i++) {
+        S[k] += powA[i];
+        powA[i] *= A[i];
+    }
+    S[k] *= bc.finv(k);
+  }
+  for (int k = 1; k <= K; k++){
+    mint res = 0;
+    for (int i = 0; i <= k; i++){
+      res += S[i] * S[k-i];
+    }
+    res = (res - S[k] * modpow(mint(2), k)) * bc.fact(k) / 2;
+    cout << res << endl;
+  }
+}
