fd5c5a00fc6b4e31f9ed6c1f0419e5e56cf739b3
Hayashi Hironori <hayaten415@gmail.com>
2020-11-09 20:01:02 +0900

    ABC046 D

---
+++ AtCoder/ABC/ABC046/D.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+#define rep(i, n) for (int i = 0; i < (n); i++)
+#define rrep(i, n) for (int i = (n - 1); i >= 0; i--)
+#define ALL(v) v.begin(), v.end()
+using namespace std;
+using P = pair<int, int>;
+typedef long long ll;
+template<class T> inline bool chmax(T& a, T b) { if (a < b) { a = b; return 1; } return 0; }
+template<class T> inline bool chmin(T& a, T b) { if (a > b) { a = b; return 1; } return 0; }
+const int dx[4] = {1, 0, -1, 0};
+const int dy[4] = {0, 1, 0, -1};
+
+int main() {
+  string s;
+  cin >> s;
+  int n = s.size();
+  int g = 0, p = 0;
+  rep(i, n){
+    if(s[i] == 'g')g++;
+    else p++;
+  }
+  cout << g - (n + 1) / 2<< endl;
+}
