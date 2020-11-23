2338132a2dc9189928731fd45bdbf49db2140f73
Hayashi Hironori <hayaten415@gmail.com>
2020-11-23 21:07:35 +0900

    ARC007 B

---
+++ AtCoder/ARC/007/B.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+#define rep(i, n) for (int i = 0; i < (n); i++)
+#define rrep(i, n) for (int i = (n - 1); i >= 0; i--)
+#define ALL(v) v.begin(), v.end()
+using namespace std;
+using P = pair<int, int>;
+typedef long long ll;
+template<class T> inline bool chmax(T& a, T b) { if (a < b) { a = b; return 1; } return 0; }
+template<class T> inline bool chmin(T& a, T b) { if (a > b) { a = b; return 1; } return 0; }
+const int dx[4] = {1, 0, -1, 0};
+const int dy[4] = {0, 1, 0, -1};
+
+int main() {
+  int n, m;
+  cin >> n >> m;
+  vector<int> A(m), B(n);
+  rep(i, m) cin >> A[i];
+  rep(i, n) B[i] = i + 1;
+  int first = 0;
+  rep(i, m){
+    rep(j, n){
+      if(A[i] == B[j]){
+        swap(first, B[j]);
+        break;
+      }
+    }
+  }
+  rep(i, n){
+    cout << B[i] << endl;
+  }
+}
