814dc25bd93acdd4579e7b8e315add2f6aefd814
Hayashi Hironori <hayaten415@gmail.com>
2020-12-12 22:53:15 +0900

    ABC106 C

---
+++ AtCoder/ABC/ABC106/C.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+#define rep(i, n) for (int i = 0; i < (n); i++)
+#define rrep(i, n) for (int i = (n - 1); i >= 0; i--)
+#define ALL(v) v.begin(), v.end()
+using namespace std;
+using P = pair<int, int>;
+typedef long long ll;
+template<class T> inline bool chmax(T& a, T b) { if (a < b) { a = b; return 1; } return 0; }
+template<class T> inline bool chmin(T& a, T b) { if (a > b) { a = b; return 1; } return 0; }
+const int dx[4] = {1, 0, -1, 0};
+const int dy[4] = {0, 1, 0, -1};
+
+int main() {
+  string s;
+  cin >> s;
+  ll k;
+  cin >> k;
+  int n = s.size();
+  int cn = 0;
+  rep(i, n){
+    if(s[i] == '1')cn++;
+    else break;
+  }
+  if(k <= cn){
+    cout << 1 << endl;
+  }else {
+    cout << s[cn] << endl;
+  }
+}
