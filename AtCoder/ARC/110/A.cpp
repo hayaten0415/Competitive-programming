6b2403ee3739f6f9ccdc0169e42052c3d2699eb6
Hayashi Hironori <hayaten415@gmail.com>
2020-12-22 14:12:47 +0900

    ARC110 A

---
+++ AtCoder/ARC/110/A.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+#define rep(i, n) for (int i = 0; i < (n); i++)
+#define rrep(i, n) for (int i = (n - 1); i >= 0; i--)
+#define ALL(v) v.begin(), v.end()
+using namespace std;
+using P = pair<int, int>;
+typedef long long ll;
+template<class T> inline bool chmax(T& a, T b) { if (a < b) { a = b; return 1; } return 0; }
+template<class T> inline bool chmin(T& a, T b) { if (a > b) { a = b; return 1; } return 0; }
+const int dx[4] = {1, 0, -1, 0};
+const int dy[4] = {0, 1, 0, -1};
+
+ll lcm(ll a, ll b) {
+  return a * b / gcd(a, b);
+}
+
+int main() {
+  ll n;
+  cin >> n;
+  ll d = 2;
+  for (ll i = 2; i <= n; i++){
+    d = lcm(d, i);
+  }
+  d++;
+  cout << d << endl;
+}
