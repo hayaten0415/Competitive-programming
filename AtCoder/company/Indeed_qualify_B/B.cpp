b59123b71807224c4f6cabacc85ef31e0b783231
Hayashi Hironori <hayaten415@gmail.com>
2021-01-16 20:24:06 +0900

    Indeed qualifyB

---
+++ AtCoder/company/Indeed_qualify_B/B.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+#define rep(i, n) for (int i = 0; i < (n); i++)
+#define rrep(i, n) for (int i = (n - 1); i >= 0; i--)
+#define ALL(v) v.begin(), v.end()
+using namespace std;
+using P = pair<int, int>;
+typedef long long ll;
+template<class T> inline bool chmax(T& a, T b) { if (a < b) { a = b; return 1; } return 0; }
+template<class T> inline bool chmin(T& a, T b) { if (a > b) { a = b; return 1; } return 0; }
+const int dx[4] = {1, 0, -1, 0};
+const int dy[4] = {0, 1, 0, -1};
+
+int main() {
+  string s, t;
+  cin >> s >> t;
+  int n = s.size();
+  int k = t.size();
+  if(n != k){
+    cout << -1 << endl;
+    return 0;
+  }
+  rep(i, n){
+    bool ok = true;
+    rep(j, n){
+      int d = (i + j) % n;
+      if(s[j] != t[d])ok = false;
+    }
+    if(ok){
+      cout << i << endl;
+      return 0;
+    }
+  }
+  cout << -1 << endl;
+}
