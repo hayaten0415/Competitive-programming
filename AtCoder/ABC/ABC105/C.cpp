814779b5f5061ba1921836bed54bb5748ce6c097
Hayashi Hironori <hayaten415@gmail.com>
2021-01-17 17:24:29 +0900

    ABC105 C

---
+++ AtCoder/ABC/ABC105/C.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+#define rep(i, n) for (int i = 0; i < (n); i++)
+#define rrep(i, n) for (int i = (n - 1); i >= 0; i--)
+#define ALL(v) v.begin(), v.end()
+using namespace std;
+using P = pair<int, int>;
+typedef long long ll;
+template<class T> inline bool chmax(T& a, T b) { if (a < b) { a = b; return 1; } return 0; }
+template<class T> inline bool chmin(T& a, T b) { if (a > b) { a = b; return 1; } return 0; }
+const int dx[4] = {1, 0, -1, 0};
+const int dy[4] = {0, 1, 0, -1};
+
+ll two[51];
+
+int main() {
+  ll n;
+  cin >> n;
+  if(n == 0){
+    cout << 0 << endl;
+    return 0;
+  }
+  string ans = "";
+  while(n != 0){
+    int d = n % 2;
+    if(d < 0)d += 2;
+    n = (n - d) / (-2);
+    ans += char('0' + d);
+  }
+  reverse(ALL(ans));
+  cout << ans << endl;
+}
