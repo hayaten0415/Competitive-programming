8ace57c9f9ec0bc7f60b8e29336cf15fdc996226
Hayashi Hironori <hayaten415@gmail.com>
2021-01-24 15:10:06 +0900

    AGC021 A

---
+++ AtCoder/AGC/012/A.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+#define rep(i, n) for (int i = 0; i < (n); i++)
+#define rrep(i, n) for (int i = (n - 1); i >= 0; i--)
+#define ALL(v) v.begin(), v.end()
+using namespace std;
+using P = pair<int, int>;
+typedef long long ll;
+template<class T> inline bool chmax(T& a, T b) { if (a < b) { a = b; return 1; } return 0; }
+template<class T> inline bool chmin(T& a, T b) { if (a > b) { a = b; return 1; } return 0; }
+const int dx[4] = {1, 0, -1, 0};
+const int dy[4] = {0, 1, 0, -1};
+
+int main() {
+  int n;
+  cin >> n;
+  vector<ll> A(3 * n);
+  rep(i, 3 * n) cin >> A[i];
+  sort(ALL(A), greater{});
+  ll ans = 0;
+  for (int i = 1; i < 2 * n; i+= 2){
+    ans += A[i];
+  }
+  cout << ans << endl;
+}
