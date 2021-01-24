6decba99ddd212622e90ba7a132cf80795d502c3
Hayashi Hironori <hayaten415@gmail.com>
2021-01-24 21:34:26 +0900

    ARC067 A

---
+++ AtCoder/ARC/067/A.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+#define rep(i, n) for (int i = 0; i < (n); i++)
+#define rrep(i, n) for (int i = (n - 1); i >= 0; i--)
+#define ALL(v) v.begin(), v.end()
+using namespace std;
+using P = pair<int, int>;
+typedef long long ll;
+template<class T> inline bool chmax(T& a, T b) { if (a < b) { a = b; return 1; } return 0; }
+template<class T> inline bool chmin(T& a, T b) { if (a > b) { a = b; return 1; } return 0; }
+const int dx[4] = {1, 0, -1, 0};
+const int dy[4] = {0, 1, 0, -1};
+
+int main() {
+  ll A, K;
+  cin >> A >> K;
+  if(K == 0){
+    cout << 2000000000000 - A << endl;
+    return 0;
+  }
+  ll num = A;
+  int ans = 0;
+  while (num < 2000000000000){
+    ans++;
+    num += K * (num) + 1;
+  }
+  cout << ans << endl;
+}
