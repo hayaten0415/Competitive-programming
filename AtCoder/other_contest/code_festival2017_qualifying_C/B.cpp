44c2c14de90386c2c51be40aeef3c4d619acbea9
Hayashi Hironori <hayaten415@gmail.com>
2021-01-25 23:14:26 +0900

    code festival2017 B

---
+++ AtCoder/other_contest/code_festival2017_qualifying_C/B.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+#define rep(i, n) for (int i = 0; i < (n); i++)
+#define rrep(i, n) for (int i = (n - 1); i >= 0; i--)
+#define ALL(v) v.begin(), v.end()
+using namespace std;
+using P = pair<int, int>;
+typedef long long ll;
+template<class T> inline bool chmax(T& a, T b) { if (a < b) { a = b; return 1; } return 0; }
+template<class T> inline bool chmin(T& a, T b) { if (a > b) { a = b; return 1; } return 0; }
+const int dx[4] = {1, 0, -1, 0};
+const int dy[4] = {0, 1, 0, -1};
+
+int main() {
+  int n;
+  cin >> n;
+  ll ans = 1;
+  rep(i, n) ans *= 3;
+  ll rest = 1;
+  rep(i, n){
+    ll a;
+    cin >> a;
+    if(a % 2 == 0)rest *= 2;
+  }
+  cout << ans - rest << endl;
+}
