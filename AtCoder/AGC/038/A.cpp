bf9bb27c756cebde177ef9e99d9fcd69ba5ab87a
Hayashi Hironori <hayaten415@gmail.com>
2021-02-28 22:38:13 +0900

    AGC038 A

---
+++ AtCoder/AGC/038/A.cpp
@@ -0,0 +1,33 @@
+#pragma region Macros
+// #pragma GCC target("avx2")
+#pragma GCC optimize("O3")
+#include <bits/stdc++.h>
+#define rep(i, n) for (int i = 0; i < (n); i++)
+#define rrep(i, n) for (int i = (n - 1); i >= 0; i--)
+#define ALL(v) v.begin(), v.end()
+#define pb push_back
+#define eb emplace_back
+using namespace std;
+using P = pair<int, int>;
+typedef long long ll;
+template<class T> inline bool chmax(T& a, T b) { if (a < b) { a = b; return 1; } return 0; }
+template<class T> inline bool chmin(T& a, T b) { if (a > b) { a = b; return 1; } return 0; }
+const int dx[4] = {1, 0, -1, 0};
+const int dy[4] = {0, 1, 0, -1};
+
+int main() {
+  int H, W, A, B;
+  cin >> H >> W >> A >> B;
+  rep(i, H){
+    rep(j, W){
+      if(i < H - B){
+        if(j < W- A)cout << 0;
+        else cout << 1;
+      }else{
+        if(j < W- A)cout << 1;
+        else cout << 0;
+      }
+    }
+    cout << endl;
+  }
+}
