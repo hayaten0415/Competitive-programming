7f5803e831b9f4ddab66b06335aa38bd249c558b
Hayashi Hironori <hayaten415@gmail.com>
2021-02-28 21:41:54 +0900

    ABC103 D

---
+++ AtCoder/ABC/ABC103/D.cpp
@@ -0,0 +1,34 @@
+#pragma region Macros
+// #pragma GCC target("avx2")
+#pragma GCC optimize("O3")
+#include <bits/stdc++.h>
+#define rep(i, n) for (int i = 0; i < (n); i++)
+#define rrep(i, n) for (int i = (n - 1); i >= 0; i--)
+#define ALL(v) v.begin(), v.end()
+#define pb push_back
+#define eb emplace_back
+using namespace std;
+using P = pair<int, int>;
+typedef long long ll;
+template<class T> inline bool chmax(T& a, T b) { if (a < b) { a = b; return 1; } return 0; }
+template<class T> inline bool chmin(T& a, T b) { if (a > b) { a = b; return 1; } return 0; }
+const int dx[4] = {1, 0, -1, 0};
+const int dy[4] = {0, 1, 0, -1};
+
+int main() {
+  int n, m;
+  cin >> n >> m;
+  vector<P> ab(m);
+  rep(i, m) cin >> ab[i].second >> ab[i].first;
+  sort(ALL(ab));
+  int ans = 0;
+  int limit = 0;
+  rep(i, m){
+    if(ab[i].second <= limit)continue;
+    else {
+      limit = ab[i].first - 1;
+      ans++;
+    }
+  }
+  cout << ans << endl;
+}
