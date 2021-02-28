3ba280676b559c0bccdc6c06a10f6bfa971b7880
Hayashi Hironori <hayaten415@gmail.com>
2021-02-28 14:59:30 +0900

    Repeat self-multipication

---
+++ AtCoder/lib/Repeat_self-multipication.cpp
@@ -0,0 +1,38 @@
+#pragma region Macros
+// #pragma GCC target("avx2")
+#pragma GCC optimize("O3")
+#include <bits/stdc++.h>
+#include <atcoder/all>
+#define rep(i, n) for (int i = 0; i < (n); i++)
+#define rrep(i, n) for (int i = (n - 1); i >= 0; i--)
+#define ALL(v) v.begin(), v.end()
+#define pb push_back
+#define eb emplace_back
+using namespace std;
+using namespace atcoder;
+using P = pair<int, int>;
+typedef long long ll;
+template<class T> inline bool chmax(T& a, T b) { if (a < b) { a = b; return 1; } return 0; }
+template<class T> inline bool chmin(T& a, T b) { if (a > b) { a = b; return 1; } return 0; }
+const int dx[4] = {1, 0, -1, 0};
+const int dy[4] = {0, 1, 0, -1};
+const ll INF = (1LL << 60);
+
+ll mypow(ll x, ll n){
+  if(n == 0)
+    return 1;
+  if(n % 2 == 0)
+    return mypow(x * x, n / 2);
+  else
+    return x * mypow(x, n - 1);
+}
+
+long long modpow(long long a, long long n, long long mod) {
+    long long res = 1;
+    while (n > 0) {
+        if (n & 1) res = res * a % mod;
+        a = a * a % mod;
+        n >>= 1;
+    }
+    return res;
+}
