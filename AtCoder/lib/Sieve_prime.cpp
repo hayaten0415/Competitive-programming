ad9f75eb619bff912f556cdaa9876982ffd76ca8
hayaten0415 <hayaten415@gmail.com>
2021-03-26 11:57:00 +0900

    SievePrime Library add divisors

--- AtCoder/lib/Sieve_prime.cpp
+++ AtCoder/lib/Sieve_prime.cpp
@@ -1,7 +1,12 @@
+#pragma region Macros
+// #pragma GCC target("avx2")
+#pragma GCC optimize("O3")
 #include <bits/stdc++.h>
 #define rep(i, n) for (int i = 0; i < (n); i++)
 #define rrep(i, n) for (int i = (n - 1); i >= 0; i--)
 #define ALL(v) v.begin(), v.end()
+#define pb push_back
+#define eb emplace_back
 using namespace std;
 using P = pair<int, int>;
 typedef long long ll;
@@ -9,6 +14,9 @@ template<class T> inline bool chmax(T& a, T b) { if (a < b) { a = b; return 1; }
 template<class T> inline bool chmin(T& a, T b) { if (a > b) { a = b; return 1; } return 0; }
 const int dx[4] = {1, 0, -1, 0};
 const int dy[4] = {0, 1, 0, -1};
+const int fx[8] = {0, 1, 1, 1, 0, -1, -1, -1};
+const int fy[8] = {1, 1, 0, -1, -1, -1, 0, 1};
+
 
 struct Sieve_prime{
   vector<int> f, primes;
@@ -48,4 +56,17 @@ struct Sieve_prime{
     }
     return res;
   }
+  vector<int> divisors(int x){
+    assert(x > 0);
+    vector<int> res = {1};
+    for(auto [p, e] : factor(x)){
+      int n = res.size() * e;
+      for(int i=0; i<n; ++i){
+        res.push_back(res[i] * p);
+      }
+    }
+    return res;
+  }
 };
+
+Sieve_prime Sieve(2e5 + 1);
