637ed1d441c50e9bd07e710e17d6587c1f140dc5
hayaten0415 <hayaten415@gmail.com>
2021-03-26 11:55:10 +0900

    Codeforces div3 697 G

---
+++ Codeforces/div3/697/editional_G.cpp
@@ -0,0 +1,99 @@
+#pragma region Macros
+// #pragma GCC target("avx2")
+#pragma GCC optimize("O3")
+#include <bits/stdc++.h>
+#define rep(i, n) for (int i = 0; i < (n); i++)
+#define rrep(i, n) for (int i = (n - 1); i >= 0; i--)
+#define ALL(v) v.begin(), v.end()
+#define pb push_back
+#define eb emplace_back
+using namespace std;
+using P = pair<int, int>;
+typedef long long ll;
+template<class T> inline bool chmax(T& a, T b) { if (a < b) { a = b; return 1; } return 0; }
+template<class T> inline bool chmin(T& a, T b) { if (a > b) { a = b; return 1; } return 0; }
+const int dx[4] = {1, 0, -1, 0};
+const int dy[4] = {0, 1, 0, -1};
+const int fx[8] = {0, 1, 1, 1, 0, -1, -1, -1};
+const int fy[8] = {1, 1, 0, -1, -1, -1, 0, 1};
+
+
+struct Sieve_prime{
+  vector<int> f, primes;
+  int n;
+  Sieve_prime(int n = 1): n(n), f(n +1){
+    f[0] = f[1] = -1;
+    for (ll i = 2; i <= n; i++){
+      if(f[i])continue;
+      primes.emplace_back(i);
+      f[i] = i;
+      for (ll j = i * i; j <= n; j += i){
+        //この条件をつけると最小素因数が記録される
+        //つけないと最大素因数が記録される。
+        if(!f[j]) f[j] = i;
+      }
+    }
+  }
+  bool isPrime(int x) { return f[x] == x; }
+  vector<int> factorlist(int x){
+    vector<int> res;
+    while (x != 1){
+      res.emplace_back(f[x]);
+      x /= f[x];
+    }
+    return res;
+  }
+  vector<P> factor(int x) {
+    vector<int> fl = factorlist(x);
+    if (fl.size() == 0) return {};
+    vector<P> res(1, P(fl[0], 0));
+    for (int p : fl) {
+      if (res.back().first == p) {
+        res.back().second++;
+      } else {
+        res.emplace_back(p, 1);
+      }
+    }
+    return res;
+  }
+  vector<int> divisors(int x){
+    assert(x > 0);
+    vector<int> res = {1};
+    for(auto [p, e] : factor(x)){
+      int n = res.size() * e;
+      for(int i=0; i<n; ++i){
+        res.push_back(res[i] * p);
+      }
+    }
+    return res;
+  }
+};
+
+vector<int> dp, cn;
+
+void solve() {
+  int n;
+  cin >> n;
+  vector<int> A(n);
+  cn.assign(2e5 + 1, 0);
+  dp.assign(2e5 + 1, 0);
+  rep(i, n){
+    cin >> A[i];
+    cn[A[i]]++;
+  }
+  int ans = 0;
+  for(int i = 1; i <= 2e5; i++) {
+    dp[i] += cn[i];
+    chmax(ans, dp[i]);
+    for(int j = 2 * i; j <= 2e5; j += i) {
+      chmax(dp[j], dp[i]);
+    }
+  }
+  cout << n - ans << endl;
+}
+
+int main() {
+  int t;
+  cin >> t;
+  while(t--) solve();
+}
---
+++ Codeforces/div3/697/memo_rec_G.cpp
@@ -0,0 +1,109 @@
+#pragma region Macros
+// #pragma GCC target("avx2")
+#pragma GCC optimize("O3")
+#include <bits/stdc++.h>
+#define rep(i, n) for (int i = 0; i < (n); i++)
+#define rrep(i, n) for (int i = (n - 1); i >= 0; i--)
+#define ALL(v) v.begin(), v.end()
+#define pb push_back
+#define eb emplace_back
+using namespace std;
+using P = pair<int, int>;
+typedef long long ll;
+template<class T> inline bool chmax(T& a, T b) { if (a < b) { a = b; return 1; } return 0; }
+template<class T> inline bool chmin(T& a, T b) { if (a > b) { a = b; return 1; } return 0; }
+const int dx[4] = {1, 0, -1, 0};
+const int dy[4] = {0, 1, 0, -1};
+const int fx[8] = {0, 1, 1, 1, 0, -1, -1, -1};
+const int fy[8] = {1, 1, 0, -1, -1, -1, 0, 1};
+
+
+struct Sieve_prime{
+  vector<int> f, primes;
+  int n;
+  Sieve_prime(int n = 1): n(n), f(n +1){
+    f[0] = f[1] = -1;
+    for (ll i = 2; i <= n; i++){
+      if(f[i])continue;
+      primes.emplace_back(i);
+      f[i] = i;
+      for (ll j = i * i; j <= n; j += i){
+        //この条件をつけると最小素因数が記録される
+        //つけないと最大素因数が記録される。
+        if(!f[j]) f[j] = i;
+      }
+    }
+  }
+  bool isPrime(int x) { return f[x] == x; }
+  vector<int> factorlist(int x){
+    vector<int> res;
+    while (x != 1){
+      res.emplace_back(f[x]);
+      x /= f[x];
+    }
+    return res;
+  }
+  vector<P> factor(int x) {
+    vector<int> fl = factorlist(x);
+    if (fl.size() == 0) return {};
+    vector<P> res(1, P(fl[0], 0));
+    for (int p : fl) {
+      if (res.back().first == p) {
+        res.back().second++;
+      } else {
+        res.emplace_back(p, 1);
+      }
+    }
+    return res;
+  }
+  vector<int> divisors(int x){
+    assert(x > 0);
+    vector<int> res = {1};
+    for(auto [p, e] : factor(x)){
+      int n = res.size() * e;
+      for(int i=0; i<n; ++i){
+        res.push_back(res[i] * p);
+      }
+    }
+    return res;
+  }
+};
+
+Sieve_prime Sieve(2e5 + 1);
+vector<int> dp, cn;
+
+int rec(int s){
+  if(s == 1)return cn[1];
+  if(dp[s] != -1)return dp[s];
+  int num = cn[s];
+  int d = 0;
+  vector<int> st = Sieve.divisors(s);
+  for(auto p : st){
+    if(p == 1)continue;
+    chmax(d, rec(s / p));
+  }
+  return dp[s] = num + d;
+}
+
+void solve() {
+  int n;
+  cin >> n;
+  vector<int> A(n);
+  cn.assign(2e5 + 1, 0);
+  dp.assign(2e5 + 1, -1);
+  rep(i, n){
+    cin >> A[i];
+    cn[A[i]]++;
+  }
+  int ans = 0;
+  rep(i, n){
+    chmax(ans, rec(A[i]));
+  }
+  cout << n - ans << endl;
+}
+
+int main() {
+  int t;
+  cin >> t;
+  while(t--) solve();
+}
