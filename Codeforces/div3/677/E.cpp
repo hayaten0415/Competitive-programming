37645e1229ec41475e5e3f47c5ffe8183e20cead
hayaten0415 <hayaten415@gmail.com>
2021-03-31 17:05:47 +0900

    Codeforces 677 E

---
+++ Codeforces/div3/677/E.cpp
@@ -0,0 +1,39 @@
+#pragma region Macros
+// #pragma GCC target("avx2")
+#pragma GCC optimize("O3")
+#include <bits/stdc++.h>
+#define rep(i, n) for (int i = 0; i < (n); i++)
+#define rrep(i, n) for (int i = (n - 1); i >= 0; i--)
+#define ALL(v) v.begin(), v.end()
+#define pb push_back
+#define eb emplace_back
+#define endl "\n"
+using namespace std;
+using P = pair<int, int>;
+using PL = pair<long long, long long>;
+typedef long long ll;
+template<class T> inline bool chmax(T& a, T b) { if (a < b) { a = b; return 1; } return 0; }
+template<class T> inline bool chmin(T& a, T b) { if (a > b) { a = b; return 1; } return 0; }
+const int dx[4] = {1, 0, -1, 0};
+const int dy[4] = {0, 1, 0, -1};
+const int fx[8] = {0, 1, 1, 1, 0, -1, -1, -1};
+const int fy[8] = {1, 1, 0, -1, -1, -1, 0, 1};
+
+
+void solve() {
+  ll n;
+  cin >> n;
+  ll ans = 1;
+  for (ll i = 1; i < n; i++)ans *= i;
+  ans *= 2;
+  ans /= n;
+  cout << ans << endl;
+}
+
+int main() {
+  cin.tie(0);
+  ios::sync_with_stdio(false);
+  int t;
+  t =1;
+  while(t--) solve();
+}
