0f95ff89d7cbf61a846bc72e7915382fbdfcb912
hayaten0415 <hayaten415@gmail.com>
2021-04-20 23:21:27 +0900

    evima Birthday day1 A

---
+++ AtCoder/other_contest/evima_Birathday_day1/A.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+#include <atcoder/all>
+#define rep(i, n) for (int i = 0; i < (n); i++)
+#define rrep(i, n) for (int i = (n - 1); i >= 0; i--)
+#define ALL(v) v.begin(), v.end()
+#define pb push_back
+#define eb emplace_back
+using namespace std;
+using namespace atcoder;
+using P = pair<int, int>;
+typedef long long ll;
+template<class T> inline bool chmax(T& a, T b) { if (a < b) { a = b; return 1; } return 0; }
+template<class T> inline bool chmin(T& a, T b) { if (a > b) { a = b; return 1; } return 0; }
+const int dx[4] = {1, 0, -1, 0};
+const int dy[4] = {0, 1, 0, -1};
+const int fx[8] = {0, 1, 1, 1, 0, -1, -1, -1};
+const int fy[8] = {1, 1, 0, -1, -1, -1, 0, 1};
+const ll INF = (1LL << 60);
+
+int main() {
+  ll n;
+  cin >> n;
+  ll ans = INF;
+  rep(i, 100)rep(k, 100){
+    ll num = n - 8 * i - 10 * k;
+    if(num < 0)continue;
+    if(num % 26 == 0){
+      chmin(ans, (ll)i + k);
+    }
+  }
+  cout << ans << endl;
+}
