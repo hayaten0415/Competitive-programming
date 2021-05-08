b03f683144c54160c13e18a8df3bec91e09bb07e
hayaten0415 <hayaten415@gmail.com>
2021-05-08 18:48:21 +0900

    Codeforces 719 A-E

---
+++ Codeforces/div3/719/B.cpp
@@ -0,0 +1,49 @@
+#pragma region Macros
+// #pragma GCC target("avx2")
+#pragma GCC optimize("O3")
+#include <bits/stdc++.h>
+#define rep(i, n) for (int i = 0; i < (n); i++)
+#define rrep(i, n) for (int i = (n - 1); i >= 0; i--)
+#define ALL(v) v.begin(), v.end()
+#define pb push_back
+#define eb emplace_back
+#define endl "\n"
+#define popcount(bit) __builtin_popcount(bit)
+using namespace std;
+using P = pair<int, int>;
+using PL = pair<long long, long long>;
+typedef long long ll;
+template<class T> inline bool chmax(T& a, T b) { if (a < b) { a = b; return 1; } return 0; }
+template<class T> inline bool chmin(T& a, T b) { if (a > b) { a = b; return 1; } return 0; }
+const int dx[4] = {1, 0, -1, 0};
+const int dy[4] = {0, 1, 0, -1};
+const int fx[8] = {0, 1, 1, 1, 0, -1, -1, -1};
+const int fy[8] = {1, 1, 0, -1, -1, -1, 0, 1};
+
+
+void solve() {
+  int n;
+  cin >> n;
+  string k = to_string(n);
+  int d = n;
+  int cn = 0;
+  while(d > 0){
+    cn++;
+    d /= 10;
+  }
+  int ans = 9 * (cn - 1);
+  int s = k[0] - '0';
+  ans += (s - 1);
+  string f = string(cn, k[0]);
+  int g = stoi(f);
+  if(n >= g)ans++;
+  cout << ans << endl;
+}
+
+int main() {
+  cin.tie(0);
+  ios::sync_with_stdio(false);
+  int t;
+  cin >> t;
+  while(t--) solve();
+}
