86145eef727e53db340fbc8012c8e88449bd6c3d
hayaten0415 <hayaten415@gmail.com>
2021-05-13 09:56:12 +0900

    MBC002 A,B

---
+++ MojaCoder/MBC/002/A.cpp
@@ -0,0 +1,39 @@
+#pragma region Macros
+// #pragma GCC target("avx2")
+#pragma GCC optimize("O3")
+#include <bits/stdc++.h>
+#define rep(i, n) for (int i = 0; i < (n); i++)
+#define rrep(i, n) for (int i = (n - 1); i >= 0; i--)
+#define ALL(v) v.begin(), v.end()
+#define pb push_back
+#define eb emplace_back
+#define endl "\n"
+#define popcount(bit) __builtin_popcount(bit)
+using namespace std;
+using P = pair<int, int>;
+using PL = pair<long long, long long>;
+typedef long long ll;
+template<class T> inline bool chmax(T& a, T b) { if (a < b) { a = b; return 1; } return 0; }
+template<class T> inline bool chmin(T& a, T b) { if (a > b) { a = b; return 1; } return 0; }
+const int dx[4] = {1, 0, -1, 0};
+const int dy[4] = {0, 1, 0, -1};
+const int fx[8] = {0, 1, 1, 1, 0, -1, -1, -1};
+const int fy[8] = {1, 1, 0, -1, -1, -1, 0, 1};
+
+
+void solve() {
+  string s;
+  cin >> s;
+  int n = s.size();
+  int ans = 0;
+  rep(i, n-3){
+    if(s[i] == 'n' && s[i+1] == 'y' && s[i+2] == 'a' && s[i+3] == 'n')ans++;
+  }
+  cout << ans << endl;
+}
+
+int main() {
+  cin.tie(0);
+  ios::sync_with_stdio(false);
+  solve();
+}
