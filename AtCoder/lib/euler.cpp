3755c55adbadd2e7a842af967faa781eb4d0d5ef
hayaten0415 <hayaten415@gmail.com>
2021-05-13 11:20:09 +0900

    euler library

---
+++ AtCoder/lib/euler.cpp
@@ -0,0 +1,45 @@
+#include <bits/stdc++.h>
+#define rep(i, n) for (int i = 0; i < (n); i++)
+#define rrep(i, n) for (int i = (n - 1); i >= 0; i--)
+#define ALL(v) v.begin(), v.end()
+#define pb push_back
+#define eb emplace_back
+#define endl "\n"
+#define popcount(bit) __builtin_popcount(bit)
+using namespace std;
+using P = pair<int, int>;
+using PL = pair<long long, long long>;
+typedef long long ll;
+template<class T> inline bool chmax(T& a, T b) { if (a < b) { a = b; return 1; } return 0; }
+template<class T> inline bool chmin(T& a, T b) { if (a > b) { a = b; return 1; } return 0; }
+const int dx[4] = {1, 0, -1, 0};
+const int dy[4] = {0, 1, 0, -1};
+const int fx[8] = {0, 1, 1, 1, 0, -1, -1, -1};
+const int fy[8] = {1, 1, 0, -1, -1, -1, 0, 1};
+
+vector<int> euler_phi_table(int n) {
+  vector< int > euler(n + 1);
+  for(int i = 0; i <= n; i++) {
+    euler[i] = i;
+  }
+  for(int i = 2; i <= n; i++) {
+    if(euler[i] == i) {
+      for(int j = i; j <= n; j += i) {
+        euler[j] = euler[j] / i * (i - 1);
+      }
+    }
+  }
+  return euler;
+}
+
+ll euler_phi(ll n) {
+  ll ret = n;
+  for(ll i = 2; i * i <= n; i++) {
+    if(n % i == 0) {
+      ret -= ret / i;
+      while(n % i == 0) n /= i;
+    }
+  }
+  if(n > 1) ret -= ret / n;
+  return ret;
+}
