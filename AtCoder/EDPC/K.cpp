d33e388b194760c06802f67fa8b96abb48d91f43
hayaten0415 <hayaten415@gmail.com>
2021-06-01 19:44:32 +0900

    EDPC M,K

---
+++ AtCoder/EDPC/K.cpp
@@ -0,0 +1,41 @@
+#pragma region Macros
+// #pragma GCC target("avx2")
+#pragma GCC optimize("O3")
+#include <bits/stdc++.h>
+#include <atcoder/all>
+#define rep(i, n) for (int i = 0; i < (n); i++)
+#define rrep(i, n) for (int i = (n - 1); i >= 0; i--)
+#define ALL(v) v.begin(), v.end()
+#define endl "\n"
+#define popcount(bit) __builtin_popcount(bit)
+#define pb push_back
+#define eb emplace_back
+using namespace std;
+using namespace atcoder;
+using P = pair<int, int>;
+typedef long long ll;
+template<class T> inline bool chmax(T& a, T b) { if (a < b) { a = b; return 1; } return 0; }
+template<class T> inline bool chmin(T& a, T b) { if (a > b) { a = b; return 1; } return 0; }
+const int dx[4] = {1, 0, -1, 0};
+const int dy[4] = {0, 1, 0, -1};
+const int fx[8] = {0, 1, 1, 1, 0, -1, -1, -1};
+const int fy[8] = {1, 1, 0, -1, -1, -1, 0, 1};
+
+
+int main() {
+  int n, k;
+  cin >> n >> k;
+  vector<int> A(n);
+  rep(i, n) cin >> A[i];
+  vector<int> dp(k + 1, 0);
+  rep(i, k+1)rep(j, n){
+    if(i - A[j] < 0)continue;
+    if(dp[i - A[j]] != 0)continue;
+    dp[i] = 1;
+  }
+  if(dp[k]){
+    cout << "First" << endl;
+  }else{
+    cout << "Second" << endl;
+  }
+}
