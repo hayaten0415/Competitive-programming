6773de1a4a7e3f9200c1cbf4dce570271d582eda
hayaten0415 <hayaten415@gmail.com>
2021-06-15 15:44:16 +0900

    ARC056 C

---
+++ AtCoder/ARC/056/C_subtask.cpp
@@ -0,0 +1,56 @@
+#pragma region Macros
+// #pragma GCC target("avx2")
+#pragma GCC optimize("O3")
+#include <bits/stdc++.h>
+#include <atcoder/all>
+#define rep(i, n) for (int i = 0; i < (n); i++)
+#define rrep(i, n) for (int i = (n - 1); i >= 0; i--)
+#define ALL(v) v.begin(), v.end()
+#define endl "\n"
+#define popcount(bit) __builtin_popcount(bit)
+#define pb push_back
+#define eb emplace_back
+using namespace std;
+using namespace atcoder;
+using P = pair<int, int>;
+typedef long long ll;
+template<class T> inline bool chmax(T& a, T b) { if (a < b) { a = b; return 1; } return 0; }
+template<class T> inline bool chmin(T& a, T b) { if (a > b) { a = b; return 1; } return 0; }
+const int dx[4] = {1, 0, -1, 0};
+const int dy[4] = {0, 1, 0, -1};
+const int fx[8] = {0, 1, 1, 1, 0, -1, -1, -1};
+const int fy[8] = {1, 1, 0, -1, -1, -1, 0, 1};
+const ll INF = (1LL << 60);
+ll d[17][17];
+
+int main() {
+  int n;
+  ll k;
+  cin >> n >> k;
+  rep(i, n) rep(j, n) cin >> d[i][j];
+  int n2 = (1 << n);
+  vector<ll> cost(n2);
+  rep(i, n2){
+    rep(j, n) for (int s = j + 1; s < n;s++){
+      if((i & (1 << j)) && (i & (1 << s))){
+        cost[i] += d[j][s];
+      }
+    }
+  }
+  vector<vector<ll>> dp(n + 1, vector<ll>(n2, INF));
+  dp[0][0] = 0;
+  for (int i = 1; i <= n; i++){
+    rep(S, n2){
+      //集合Sの部分集合のうち空集合は飛ばしている。
+      for (int T = S; T > 0; T = (T - 1) & S){
+        int rest = S ^ T;
+        chmin(dp[i][S], (dp[i-1][rest] + cost[S] - cost[T] - cost[rest]));
+      }
+    }
+  }
+  ll ans = -INF;
+  for (ll i = 1; i <= n; i++){
+    chmax(ans, i * k - dp[i][n2 - 1]);
+  }
+  cout << ans << endl;
+}
