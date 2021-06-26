152f85dc898dc73f9c2d41815050ab59d000c7c4
hayaten0415 <hayaten415@gmail.com>
2021-06-26 20:35:35 +0900

    yukicoder A-D

---
+++ yukicoder/nada2021bunka_day1/C.cpp
@@ -0,0 +1,34 @@
+#pragma region Macros
+// #pragma GCC target("avx2")
+#pragma GCC optimize("O3")
+#include <bits/stdc++.h>
+#define rep(i, n) for (int i = 0; i < (n); i++)
+#define rrep(i, n) for (int i = (n - 1); i >= 0; i--)
+#define ALL(v) v.begin(), v.end()
+#define endl "\n"
+#define popcount(bit) __builtin_popcount(bit)
+#define pb push_back
+#define eb emplace_back
+using namespace std;
+using P = pair<int, int>;
+typedef long long ll;
+template<class T> inline bool chmax(T& a, T b) { if (a < b) { a = b; return 1; } return 0; }
+template<class T> inline bool chmin(T& a, T b) { if (a > b) { a = b; return 1; } return 0; }
+const int dx[4] = {1, 0, -1, 0};
+const int dy[4] = {0, 1, 0, -1};
+const int fx[8] = {0, 1, 1, 1, 0, -1, -1, -1};
+const int fy[8] = {1, 1, 0, -1, -1, -1, 0, 1};
+
+int main() {
+  int n;
+  cin >> n;
+  vector<ll> A(2 * n);
+  rep(i, 2 * n) cin >> A[i];
+  sort(ALL(A));
+  ll ans = 0;
+  rep(i, 2 * n){
+    ans += (A[i] * A[i + 1]);
+    i++;
+  }
+  cout << ans << endl;
+}
---
+++ yukicoder/nada2021bunka_day1/D.cpp
@@ -0,0 +1,49 @@
+#include <bits/stdc++.h>
+#define rep(i, n) for (int i = 0; i < (n); i++)
+#define rrep(i, n) for (int i = (n - 1); i >= 0; i--)
+#define ALL(v) v.begin(), v.end()
+using namespace std;
+using P = pair<int, int>;
+using Graph = vector<vector<int>>;
+typedef long long ll;
+template<class T> inline bool chmax(T& a, T b) { if (a < b) { a = b; return 1; } return 0; }
+template<class T> inline bool chmin(T& a, T b) { if (a > b) { a = b; return 1; } return 0; }
+const int dx[4] = {1, 0, -1, 0};
+const int dy[4] = {0, 1, 0, -1};
+const int INF = (1 << 30);
+Graph G;
+
+int bfs(int s, int n){
+  vector<int> dist(n, INF);
+  vector<int> pre(n, -1);
+  dist[s] = 0;
+  queue<int> que;
+  que.push(s);
+  while (!que.empty()){
+    int v = que.front();
+    que.pop();
+    for(int u: G[v]){
+      if(dist[u] != INF) continue;
+      dist[u] = dist[v] + 1;
+      pre[u] = v;
+      que.push(u);
+    }
+  }
+  return dist[n - 1];
+}
+
+int main() {
+  int n, m;
+  cin >> n >> m;
+  G.resize(n);
+  rep(i,m){
+    int a, b;
+    cin >> a >> b;
+    a--; b--;
+    G[a].push_back(b);
+    G[b].push_back(a);
+  }
+  int ans = bfs(0, n);
+  if(ans == INF)ans = -1;
+  cout << ans << endl;
+}
