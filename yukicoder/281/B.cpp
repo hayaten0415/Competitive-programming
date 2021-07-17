52c2fe020224012abfbddcf53bef99a24772529a
hayaten0415 <hayaten415@gmail.com>
2021-07-17 14:56:36 +0900

    yukicoder 281 B

---
+++ yukicoder/281/B.cpp
@@ -0,0 +1,61 @@
+#pragma region Macros
+// #pragma GCC target("avx2")
+#pragma GCC optimize("O3")
+#include <bits/stdc++.h>
+#include <atcoder/all>
+#define rep(i, n) for (int i = 0; i < (n); i++)
+#define rrep(i, n) for (int i = (n - 1); i >= 0; i--)
+#define ALL(v) v.begin(), v.end()
+#define endl "\n"
+#define popcount(bit) __builtin_popcount(bit)
+#define popcountll(bit) __builtin_popcountll(bit)
+#define pb push_back
+#define eb emplace_back
+using namespace std;
+using namespace atcoder;
+using P = pair<int, int>;
+using PL = pair<long long, long long>;
+using Graph = vector<vector<int>>;
+typedef long long ll;
+template<class T> inline bool chmax(T& a, T b) { if (a < b) { a = b; return 1; } return 0; }
+template<class T> inline bool chmin(T& a, T b) { if (a > b) { a = b; return 1; } return 0; }
+const int dx[4] = {1, 0, -1, 0};
+const int dy[4] = {0, 1, 0, -1};
+const int fx[8] = {0, 1, 1, 1, 0, -1, -1, -1};
+const int fy[8] = {1, 1, 0, -1, -1, -1, 0, 1};
+const ll INF = (1LL <<60);
+
+int main() {
+  int n;
+  cin >> n;
+  vector<P> A(n-1);
+  rep(i, n-1){
+    cin >> A[i].first;
+    A[i].second = i;
+  }
+  sort(ALL(A));
+  int d = 2;
+  vector<int> ans(n - 1);
+  set<int> st;
+  st.insert(1);
+  bool ok = true;
+  rep(i, n-1){
+    int k = d - A[i].first;
+    if(st.find(k) == st.end()){
+      ok = false;
+      break;
+    }else{
+      ans[A[i].second] = k;
+      st.insert(d);
+      d++;
+    }
+  }
+  if(!ok){
+    cout << "NO" << endl;
+  }else{
+    cout << "YES" << endl;
+    rep(i, n-1){
+      cout << ans[i] << endl;
+    }
+  }
+}
