66d0d7959d31130073cc558e6bfd60475bfd52f0
hayaten0415 <hayaten415@gmail.com>
2021-07-28 00:03:12 +0900

    BaB004

---
+++ MojaCoder/BaB004/F.cpp
@@ -0,0 +1,49 @@
+#pragma region Macros
+// #pragma GCC target("avx2")
+#pragma GCC optimize("O3")
+#include <bits/stdc++.h>
+#include <atcoder/all>
+#define rep(i, n) for (int i = 0; i < (n); i++)
+#define rrep(i, n) for (int i = (n - 1); i >= 0; i--)
+#define ALL(v) v.begin(), v.end()
+#define endl "\n"
+#define popcount(bit) __builtin_popcount(bit)
+#define popcountll(bit) __builtin_popcountll(bit)
+#define pb push_back
+#define eb emplace_back
+using namespace std;
+using namespace atcoder;
+using P = pair<int, int>;
+using PL = pair<long long, long long>;
+using Graph = vector<vector<int>>;
+typedef long long ll;
+template<class T> inline bool chmax(T& a, T b) { if (a < b) { a = b; return 1; } return 0; }
+template<class T> inline bool chmin(T& a, T b) { if (a > b) { a = b; return 1; } return 0; }
+const int dx[4] = {1, 0, -1, 0};
+const int dy[4] = {0, 1, 0, -1};
+const int fx[8] = {0, 1, 1, 1, 0, -1, -1, -1};
+const int fy[8] = {1, 1, 0, -1, -1, -1, 0, 1};
+const ll INF = (1LL <<60);
+
+int main() {
+  int q;
+  cin >> q;
+  rep(i, q){
+    ll a, b, k;
+    cin >> a >> b >> k;
+    while(abs(a- b) <= k && 1 <= abs(a- b)){
+      if(a > b){
+        b *= 2;
+      }else{
+        a *= 2;
+      }
+    }
+    if(a> b){
+      cout << "Win" << endl;
+    }else if(b > a){
+      cout << "Lose" << endl;
+    }else{
+      cout << "Draw" << endl;
+    }
+  }
+}
