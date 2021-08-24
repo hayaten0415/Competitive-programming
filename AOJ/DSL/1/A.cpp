16ad7f91e589190f9ce34b065bab2f978ac96d31
hayaten0415 <hayaten415@gmail.com>
2021-08-24 14:02:03 +0900

    DSL 1 A

---
+++ AOJ/DSL/1/A.cpp
@@ -0,0 +1,83 @@
+#pragma region Macros
+// #pragma GCC target("avx2")
+#pragma GCC optimize("O3")
+#include <bits/stdc++.h>
+#define rep(i, n) for (long long i = 0; i < (n); i++)
+#define rrep(i, n) for (long long i = (n - 1); i >= 0; i--)
+#define ALL(v) v.begin(), v.end()
+#define endl "\n"
+#define fi first
+#define se second
+#define popcount(bit) __builtin_popcount(bit)
+#define popcountll(bit) __builtin_popcountll(bit)
+#define pb push_back
+#define eb emplace_back
+using namespace std;
+using P = pair<int, int>;
+using PL = pair<long long, long long>;
+using Graph = vector<vector<int>>;
+typedef long long ll;
+template<class T> inline bool chmax(T& a, T b) { if (a < b) { a = b; return 1; } return 0; }
+template<class T> inline bool chmin(T& a, T b) { if (a > b) { a = b; return 1; } return 0; }
+const int dx[4] = {1, 0, -1, 0};
+const int dy[4] = {0, 1, 0, -1};
+const int fx[8] = {0, 1, 1, 1, 0, -1, -1, -1};
+const int fy[8] = {1, 1, 0, -1, -1, -1, 0, 1};
+template <typename T>
+const auto INF = numeric_limits<T>::max()/2;
+
+
+struct UnionFind {
+  vector<int> par; //par[i]:iの親の番号
+
+  //最初は全てが根であるとする。-1のときに根である。
+  UnionFind(int N) : par(N, -1){}
+
+  // データxが属する木の根を再帰で得る：root(x) = {xの木の根}
+  int root(int x) {
+    if(par[x] < 0) return x;
+    else return par[x] = root(par[x]);
+  }
+  // データxが根かどうか
+  bool cond_root(int x){
+    if(par[x] < 0) return true;
+    else return false;
+  }
+  //xとyが同じ集合に属するか
+  bool issame(int x, int y) {
+    return root(x) == root(y);
+  }
+
+  bool merge(int x, int y) {
+    x = root(x); y = root(y);
+    if(x == y) return false;
+    if(par[x] > par[y]) swap(x,y);
+    par[x] += par[y];
+    par[y] = x;
+    return true;
+  }
+
+  int size(int x) {
+    return -par[root(x)];
+  }
+};
+
+
+int main() {
+  int n, q;
+  cin >> n >> q;
+  UnionFind uf(n);
+  while (q--){
+    int t,a,b;
+    cin >> t >> a >> b;
+    if(!t){
+      uf.merge(a, b);
+    }else{
+      if(uf.issame(a, b)){
+        cout << 1 << endl;
+      }else{
+        cout << 0 << endl;
+      }
+    }
+  }
+}
