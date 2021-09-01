df303822a2781400bd10b03abc30087046a84be3
hayaten0415 <hayaten415@gmail.com>
2021-09-01 15:15:44 +0900

    codefestival2016_final C

---
+++ AtCoder/other_contest/code_festival2016_final/C.cpp
@@ -0,0 +1,68 @@
+#pragma region Macros
+// #pragma GCC target("avx2")
+#pragma GCC optimize("O3")
+#include <bits/stdc++.h>
+#include <atcoder/all>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace std;
+using namespace __gnu_pbds;
+template <typename T>
+using ordered_set =
+    tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+struct __INIT{__INIT(){cin.tie(0);ios::sync_with_stdio(false);cout<<fixed<<setprecision(15);}} __init;
+#define rep(i, n) for (int i = 0; i < (n); i++)
+#define rrep(i, n) for (long long i = (n - 1); i >= 0; i--)
+#define ALL(v) v.begin(), v.end()
+#define endl "\n"
+#define fi first
+#define se second
+#define popcount(bit) __builtin_popcount(bit)
+#define popcountll(bit) __builtin_popcountll(bit)
+#define pb push_back
+#define eb emplace_back
+using namespace atcoder;
+using P = pair<int, int>;
+using PL = pair<long long, long long>;
+using Graph = vector<vector<int>>;
+typedef long long ll;
+template<class T> inline bool chmax(T& a, T b) { if (a < b) { a = b; return 1; } return 0; }
+template<class T> inline bool chmin(T& a, T b) { if (a > b) { a = b; return 1; } return 0; }
+const int dx[4] = {1, 0, -1, 0};
+const int dy[4] = {0, 1, 0, -1};
+const int fx[8] = {0, 1, 1, 1, 0, -1, -1, -1};
+const int fy[8] = {1, 1, 0, -1, -1, -1, 0, 1};
+template <typename T>
+const auto INF = numeric_limits<T>::max()/2;
+
+int main() {
+  int n, m;
+  cin >> n >> m;
+  vector<int> lang;
+  vector<int> p[n];
+  rep(i, n){
+    int k;
+    cin >> k;
+    rep(j, k){
+      int a;
+      cin >> a;
+      a--;
+      p[i].eb(a);
+      lang.eb(a);
+    }
+  }
+  sort(ALL(lang));
+  lang.erase(unique(ALL(lang)), lang.end());
+  dsu d(n + lang.size());
+  rep(i, n){
+    for(auto e : p[i]){
+      int f = lower_bound(ALL(lang), e) - lang.begin();
+      d.merge(i, n + f);
+    }
+  }
+  if(d.size(0) == n + lang.size()){
+    cout << "YES" << endl;
+  }else{
+    cout << "NO" << endl;
+  }
+}
