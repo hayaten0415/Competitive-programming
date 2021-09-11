05e9b834fa1551819e1cf1ed34f1e066139d4217
hayaten0415 <hayaten415@gmail.com>
2021-09-11 11:18:25 +0900

    yukicoder 313 A-E

---
+++ yukicoder/313/E.cpp
@@ -0,0 +1,61 @@
+#pragma region Macros
+// #pragma GCC target("avx2")
+#pragma GCC optimize("O3")
+#include <bits/stdc++.h>
+#include <atcoder/all>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace std;
+using namespace __gnu_pbds;
+template <typename T>
+using ordered_set =
+    tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+struct __INIT{__INIT(){cin.tie(0);ios::sync_with_stdio(false);cout<<fixed<<setprecision(15);}} __init;
+#define rep(i, n) for (long long i = 0; i < (n); i++)
+#define rrep(i, n) for (long long i = (n - 1); i >= 0; i--)
+#define ALL(v) v.begin(), v.end()
+#define endl "\n"
+#define fi first
+#define se second
+#define popcount(bit) __builtin_popcount(bit)
+#define popcountll(bit) __builtin_popcountll(bit)
+#define pb push_back
+#define eb emplace_back
+using namespace atcoder;
+using P = pair<int, int>;
+using PL = pair<long long, long long>;
+using Graph = vector<vector<int>>;
+typedef long long ll;
+template<class T> inline bool chmax(T& a, T b) { if (a < b) { a = b; return 1; } return 0; }
+template<class T> inline bool chmin(T& a, T b) { if (a > b) { a = b; return 1; } return 0; }
+const int dx[4] = {1, 0, -1, 0};
+const int dy[4] = {0, 1, 0, -1};
+const int fx[8] = {0, 1, 1, 1, 0, -1, -1, -1};
+const int fy[8] = {1, 1, 0, -1, -1, -1, 0, 1};
+template <typename T>
+const auto INF = numeric_limits<T>::max()/2;
+
+int main() {
+	int n, k;
+	cin >> n >> k;
+	vector<ll> A(n), B(n);
+	Graph G(n);
+	rep(i, n){
+		cin >> A[i] >> B[i];
+		rep(j, B[i]){
+			int b;
+			cin >> b;
+			b--;
+			G[i].eb(b);
+		}
+	}
+	vector<ll> dp(n, 0);
+	rep(i, n){
+		if(i==0)continue;
+		chmax(dp[i], dp[i-1]);
+		for(auto d : G[i]){
+			chmax(dp[i], dp[d] + A[i] - A[d]);
+		}
+	}
+	cout << dp[n - 1] << endl;
+}
