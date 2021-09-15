afb725c21f57394ada6815cc7266b09be702a9b0
hayaten0415 <hayaten415@gmail.com>
2021-09-15 23:52:30 +0900

    Codeforces div3 A-D, F1

---
+++ Codeforces/div3/544/A.cpp
@@ -0,0 +1,72 @@
+#pragma region Macros
+// #pragma GCC target("avx2")
+#pragma GCC optimize("O3")
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace std;
+using namespace __gnu_pbds;
+template <typename T>
+using ordered_set =
+    tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+#define REP(i,n) for(ll i=0;i<ll(n);i++)
+#define REPD(i,n) for(ll i=n-1;i>=0;i--)
+#define FOR(i,a,b) for(ll i=a;i<=ll(b);i++)
+#define FORD(i,a,b) for(ll i=a;i>=ll(b);i--)
+#define overload4(_1,_2,_3,_4,name,...) name
+#define overload3(_1,_2,_3,name,...) name
+#define rep1(n) for(ll i=0;i<n;++i)
+#define rep2(i,n) for(ll i=0;i<n;++i)
+#define rep3(i,a,b) for(ll i=a;i<b;++i)
+#define rep4(i,a,b,c) for(ll i=a;i<b;i+=c)
+#define rep(...) overload4(__VA_ARGS__,rep4,rep3,rep2,rep1)(__VA_ARGS__)
+#define rrep1(n) for(ll i=n;i--;)
+#define rrep2(i,n) for(ll i=n;i--;)
+#define rrep3(i,a,b) for(ll i=b;i-->(a);)
+#define rrep4(i,a,b,c) for(ll i=(a)+((b)-(a)-1)/(c)*(c);i>=(a);i-=c)
+#define rrep(...) overload4(__VA_ARGS__,rrep4,rrep3,rrep2,rrep1)(__VA_ARGS__)
+#define ALL(v) v.begin(), v.end()
+#define pb push_back
+#define eb emplace_back
+#define endl "\n"
+#define fi first
+#define se second
+#define popcount(bit) __builtin_popcount(bit)
+#define popcountll(bit) __builtin_popcountll(bit)
+using P = pair<int, int>;
+using PL = pair<long long, long long>;
+using Graph = vector<vector<int>>;
+typedef long long ll;
+template<class T> inline bool chmax(T& a, T b) { if (a < b) { a = b; return 1; } return 0; }
+template<class T> inline bool chmin(T& a, T b) { if (a > b) { a = b; return 1; } return 0; }
+const int dx[4] = {1, 0, -1, 0};
+const int dy[4] = {0, 1, 0, -1};
+const int fx[8] = {0, 1, 1, 1, 0, -1, -1, -1};
+const int fy[8] = {1, 1, 0, -1, -1, -1, 0, 1};
+template <typename T>
+const auto INF = numeric_limits<T>::max() / 2;
+
+
+void solve() {
+  string s1, s2;
+  cin >> s1 >> s2;
+  ll res1 = (s1[0] - '0') * 600 + (s1[1] - '0') * 60 + (s1[3] - '0') * 10 + (s1[4] - '0');
+  ll res2 = (s2[0] - '0') * 600 + (s2[1] - '0') * 60 + (s2[3] - '0') * 10 + (s2[4] - '0');
+  ll minute = res2 - res1;
+  minute /= 2;
+  res1 += minute;
+  cout << (res1 / 600);
+  res1 %= 600;
+  cout << res1 / 60;
+  res1 %= 60;
+  cout << ":";
+  cout << res1 / 10;
+  res1 %= 10;
+  cout << res1 << endl;
+}
+
+int main() {
+  cin.tie(0);
+  ios::sync_with_stdio(false);
+  solve();
+}
---
+++ Codeforces/div3/544/F1.cpp
@@ -0,0 +1,95 @@
+#pragma region Macros
+// #pragma GCC target("avx2")
+#pragma GCC optimize("O3")
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace std;
+using namespace __gnu_pbds;
+template <typename T>
+using ordered_set =
+    tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+#define REP(i,n) for(ll i=0;i<ll(n);i++)
+#define REPD(i,n) for(ll i=n-1;i>=0;i--)
+#define FOR(i,a,b) for(ll i=a;i<=ll(b);i++)
+#define FORD(i,a,b) for(ll i=a;i>=ll(b);i--)
+#define overload4(_1,_2,_3,_4,name,...) name
+#define overload3(_1,_2,_3,name,...) name
+#define rep1(n) for(ll i=0;i<n;++i)
+#define rep2(i,n) for(ll i=0;i<n;++i)
+#define rep3(i,a,b) for(ll i=a;i<b;++i)
+#define rep4(i,a,b,c) for(ll i=a;i<b;i+=c)
+#define rep(...) overload4(__VA_ARGS__,rep4,rep3,rep2,rep1)(__VA_ARGS__)
+#define rrep1(n) for(ll i=n;i--;)
+#define rrep2(i,n) for(ll i=n;i--;)
+#define rrep3(i,a,b) for(ll i=b;i-->(a);)
+#define rrep4(i,a,b,c) for(ll i=(a)+((b)-(a)-1)/(c)*(c);i>=(a);i-=c)
+#define rrep(...) overload4(__VA_ARGS__,rrep4,rrep3,rrep2,rrep1)(__VA_ARGS__)
+#define ALL(v) v.begin(), v.end()
+#define pb push_back
+#define eb emplace_back
+#define endl "\n"
+#define fi first
+#define se second
+#define popcount(bit) __builtin_popcount(bit)
+#define popcountll(bit) __builtin_popcountll(bit)
+using P = pair<int, int>;
+using PL = pair<long long, long long>;
+using Graph = vector<vector<int>>;
+typedef long long ll;
+template<class T> inline bool chmax(T& a, T b) { if (a < b) { a = b; return 1; } return 0; }
+template<class T> inline bool chmin(T& a, T b) { if (a > b) { a = b; return 1; } return 0; }
+const int dx[4] = {1, 0, -1, 0};
+const int dy[4] = {0, 1, 0, -1};
+const int fx[8] = {0, 1, 1, 1, 0, -1, -1, -1};
+const int fy[8] = {1, 1, 0, -1, -1, -1, 0, 1};
+template <typename T>
+const auto INF = numeric_limits<T>::max() / 2;
+
+
+void solve() {
+  int n, m;
+  cin >> n >> m;
+  Graph G(n);
+  vector<int> cnt(n);
+  rep(i, m){
+    int a, b;
+    cin >> a >> b;
+    a--, b--;
+    cnt[a]++;
+    cnt[b]++;
+    G[a].pb(b);
+    G[b].pb(a);
+  }
+  int start = -1;
+  int num = 0;
+  rep(i, n) if(chmax(num, cnt[i])) start = i;
+  vector<P> ans;
+  auto bfs = [&](int x) -> vector<int>{
+    vector<int> dist(n, -1);
+    dist[x] = 0;
+    queue<int> que;
+    que.push(x);
+    while(!que.empty()){
+      int v = que.front();
+      que.pop();
+      for(auto d: G[v]){
+        if(dist[d] != -1)continue;
+        dist[d] = dist[v] + 1;
+        ans.pb({d + 1, v + 1});
+        que.push(d);
+      }
+    }
+    return dist;
+  };
+  bfs(start);
+  for(auto [a, b] : ans){
+    cout << a << " " << b << endl;
+  }
+}
+
+int main() {
+  cin.tie(0);
+  ios::sync_with_stdio(false);
+  solve();
+}
