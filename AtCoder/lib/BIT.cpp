f7d88b0d8ec6d03de758d6a02f14d56c27405885
hayaten0415 <hayaten415@gmail.com>
2021-09-30 01:37:07 +0900

    BIT Inversion

--- AtCoder/lib/BIT.cpp
+++ AtCoder/lib/BIT.cpp
@@ -51,137 +51,32 @@ const int fy[8] = {1, 1, 0, -1, -1, -1, 0, 1};
 template <typename T>
 const auto INF = numeric_limits<T>::max()/2;
 
-namespace atcoder {
 
-namespace internal {
-
-#ifndef _MSC_VER
-template <class T>
-using is_signed_int128 =
-    typename std::conditional<std::is_same<T, __int128_t>::value ||
-                                  std::is_same<T, __int128>::value,
-                              std::true_type,
-                              std::false_type>::type;
-
-template <class T>
-using is_unsigned_int128 =
-    typename std::conditional<std::is_same<T, __uint128_t>::value ||
-                                  std::is_same<T, unsigned __int128>::value,
-                              std::true_type,
-                              std::false_type>::type;
-
-template <class T>
-using make_unsigned_int128 =
-    typename std::conditional<std::is_same<T, __int128_t>::value,
-                              __uint128_t,
-                              unsigned __int128>;
-
-template <class T>
-using is_integral = typename std::conditional<std::is_integral<T>::value ||
-                                                  is_signed_int128<T>::value ||
-                                                  is_unsigned_int128<T>::value,
-                                              std::true_type,
-                                              std::false_type>::type;
-
-template <class T>
-using is_signed_int = typename std::conditional<(is_integral<T>::value &&
-                                                 std::is_signed<T>::value) ||
-                                                    is_signed_int128<T>::value,
-                                                std::true_type,
-                                                std::false_type>::type;
-
-template <class T>
-using is_unsigned_int =
-    typename std::conditional<(is_integral<T>::value &&
-                               std::is_unsigned<T>::value) ||
-                                  is_unsigned_int128<T>::value,
-                              std::true_type,
-                              std::false_type>::type;
-
-template <class T>
-using to_unsigned = typename std::conditional<
-    is_signed_int128<T>::value,
-    make_unsigned_int128<T>,
-    typename std::conditional<std::is_signed<T>::value,
-                              std::make_unsigned<T>,
-                              std::common_type<T>>::type>::type;
-
-#else
-
-template <class T> using is_integral = typename std::is_integral<T>;
-
-template <class T>
-using is_signed_int =
-    typename std::conditional<is_integral<T>::value && std::is_signed<T>::value,
-                              std::true_type,
-                              std::false_type>::type;
-
-template <class T>
-using is_unsigned_int =
-    typename std::conditional<is_integral<T>::value &&
-                                  std::is_unsigned<T>::value,
-                              std::true_type,
-                              std::false_type>::type;
-
-template <class T>
-using to_unsigned = typename std::conditional<is_signed_int<T>::value,
-                                              std::make_unsigned<T>,
-                                              std::common_type<T>>::type;
-
-#endif
-
-template <class T>
-using is_signed_int_t = std::enable_if_t<is_signed_int<T>::value>;
-
-template <class T>
-using is_unsigned_int_t = std::enable_if_t<is_unsigned_int<T>::value>;
-
-template <class T> using to_unsigned_t = typename to_unsigned<T>::type;
-
-}  // namespace internal
-
-}  // namespace atcoder
-
-
-namespace atcoder {
-
-template <class T> struct fenwick_tree {
-    using U = internal::to_unsigned_t<T>;
-
-  public:
-    fenwick_tree() : _n(0) {}
-    explicit fenwick_tree(int n) : _n(n), data(n) {}
-
-    void add(int p, T x) {
-        assert(0 <= p && p < _n);
-        p++;
-        while (p <= _n) {
-            data[p - 1] += U(x);
-            p += p & -p;
-        }
+template <typename T>
+struct binary_indexed_tree{
+  int N;
+  vector<T> BIT;
+  binary_indexed_tree(int N): N(N), BIT(N + 1, 0){
+  }
+  void add(int i, T x){
+    i++;
+    while (i <= N){
+      BIT[i] += x;
+      i += i & -i;
     }
-
-    T sum(int l, int r) {
-        assert(0 <= l && l <= r && r <= _n);
-        return sum(r) - sum(l);
+  }
+  T sum(int i){
+    T ans = 0;
+    while (i > 0){
+      ans += BIT[i];
+      i -= i & -i;
     }
-
-  private:
-    int _n;
-    std::vector<U> data;
-
-    U sum(int r) {
-        U s = 0;
-        while (r > 0) {
-            s += data[r - 1];
-            r -= r & -r;
-        }
-        return s;
-    }
+    return ans;
+  }
+  T sum(int L, int R){
+    return sum(R) - sum(L);
+  }
 };
-
-}  // namespace atcoder
-using namespace atcoder;
 
 int main(){
   
