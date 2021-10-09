77a1b8a458619fdec61be2081ad523ee60e22522
hayaten0415 <hayaten415@gmail.com>
2021-10-10 00:26:35 +0900

    Graph BFS

--- AtCoder/lib/Graph_BFS.cpp
+++ AtCoder/lib/Graph_BFS.cpp
@@ -1,40 +1,59 @@
+#pragma region Macros
+// #pragma GCC target("avx2")
+#pragma GCC optimize("O3")
 #include <bits/stdc++.h>
-#define rep(i, n) for (int i = 0; i < (n); i++)
-#define rrep(i, n) for (int i = (n - 1); i >= 0; i--)
-#define ALL(v) v.begin(), v.end()
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
 using namespace std;
+using namespace __gnu_pbds;
+template <typename T>
+using ordered_set =
+    tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+#define REP(i,n) for(ll i=0;i<ll(n);i++)
+#define REPD(i,n) for(ll i=n-1;i>=0;i--)
+#define FOR(i,a,b) for(ll i=a;i<=ll(b);i++)
+#define FORD(i,a,b) for(ll i=a;i>=ll(b);i--)
+#define overload4(_1,_2,_3,_4,name,...) name
+#define overload3(_1,_2,_3,name,...) name
+#define rep1(n) for(ll i=0;i<n;++i)
+#define rep2(i,n) for(ll i=0;i<n;++i)
+#define rep3(i,a,b) for(ll i=a;i<b;++i)
+#define rep4(i,a,b,c) for(ll i=a;i<b;i+=c)
+#define rep(...) overload4(__VA_ARGS__,rep4,rep3,rep2,rep1)(__VA_ARGS__)
+#define rrep1(n) for(ll i=n;i--;)
+#define rrep2(i,n) for(ll i=n;i--;)
+#define rrep3(i,a,b) for(ll i=b;i-->(a);)
+#define rrep4(i,a,b,c) for(ll i=(a)+((b)-(a)-1)/(c)*(c);i>=(a);i-=c)
+#define rrep(...) overload4(__VA_ARGS__,rrep4,rrep3,rrep2,rrep1)(__VA_ARGS__)
+#define ALL(v) v.begin(), v.end()
+#define pb push_back
+#define eb emplace_back
+#define endl "\n"
+#define fi first
+#define se second
+#define popcount(bit) __builtin_popcount(bit)
+#define popcountll(bit) __builtin_popcountll(bit)
+#define sz(x) ((ll)(x).size())
 using P = pair<int, int>;
+using PL = pair<long long, long long>;
 using Graph = vector<vector<int>>;
 typedef long long ll;
 template<class T> inline bool chmax(T& a, T b) { if (a < b) { a = b; return 1; } return 0; }
 template<class T> inline bool chmin(T& a, T b) { if (a > b) { a = b; return 1; } return 0; }
+inline void Yes(bool b = true) { cout << (b ? "Yes" : "No") << '\n'; }
+inline void YES(bool b = true) { cout << (b ? "YES" : "NO") << '\n'; }
+inline void OKNG(bool b = true) { cout << (b ? "OK" : "NG") << '\n'; }
 const int dx[4] = {1, 0, -1, 0};
 const int dy[4] = {0, 1, 0, -1};
-const int INF = (1 << 30);
-Graph G;
-
-void bfs(int s, int n){
-  vector<int> dist(n, INF);
-  vector<int> pre(n, -1);
-  dist[s] = 0;
-  queue<int> que;
-  que.push(s);
-  while (!que.empty()){
-    int v = que.front();
-    que.pop();
-    for(int u: G[v]){
-      if(dist[u] != INF) continue;
-      dist[u] = dist[v] + 1;
-      pre[u] = v;
-      que.push(u);
-    }
-  }
-}
+const int fx[8] = {0, 1, 1, 1, 0, -1, -1, -1};
+const int fy[8] = {1, 1, 0, -1, -1, -1, 0, 1};
+template <typename T>
+const auto INF = numeric_limits<T>::max() / 2;
 
 int main() {
   int n, m;
   cin >> n >> m;
-  G.resize(n);
+  Graph G(n);
   rep(i,m){
     int a, b;
     cin >> a >> b;
@@ -42,5 +61,31 @@ int main() {
     G[a].push_back(b);
     G[b].push_back(a);
   }
-  bfs(0, n);
+  
+  auto bfs = [&](int s, int g) -> vector<int>{
+    vector<int> dist(n, INF<int>);
+    vector<int> pre(n, -1);
+    dist[s] = 0;
+    queue<int> que;
+    que.push(s);
+    while (!que.empty()){
+      int v = que.front();
+      que.pop();
+      for(int u: G[v]){
+        if(dist[u] != INF<int>) continue;
+        dist[u] = dist[v] + 1;
+        pre[u] = v;
+        que.push(u);
+      }
+    }
+    vector<int> route;
+    int f = g;
+    while(f != s){
+      route.pb(f);
+      f = pre[f];
+    }
+    route.pb(f);
+    reverse(ALL(route));
+    return route;
+  };
 }
