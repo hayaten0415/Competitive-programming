9ce76d816bb9077ddf80194201c63483c24157fb
hayaten0415 <hayaten0415@gmail.com>
2022-09-24 19:47:11 +0900

    ABC266 D,F

---
+++ AtCoder/ABC/ABC266/F.cpp
@@ -0,0 +1,121 @@
+#include <bits/stdc++.h>
+#include <atcoder/all>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace std;
+using namespace __gnu_pbds;
+template <typename T>
+using ordered_set =
+    tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+struct __INIT{__INIT(){cin.tie(0);ios::sync_with_stdio(false);cout<<fixed<<setprecision(15);}} __init;
+#define REP(i,n) for(ll i=0;i<ll(n);i++)
+#define REPD(i,n) for(ll i=n-1;i>=0;i--)
+#define FOR(i,a,b) for(ll i=a;i<=ll(b);i++)
+#define FORD(i,a,b) for(ll i=a;i>=ll(b);i--)
+#define overload4(_1,_2,_3,_4,name,...) name
+#define overload3(_1,_2,_3,name,...) name
+#define rep1(n) for(ll i=0;i<n;++i)
+#define rep2(i,n) for(ll i=0;i<n;++i)
+#define rep3(i,a,b) for(ll i=a;i<b;++i)
+#define rep4(i,a,b,c) for(ll i=a;i<b;i+=c)
+#define rep(...) overload4(__VA_ARGS__,rep4,rep3,rep2,rep1)(__VA_ARGS__)
+#define rrep1(n) for(ll i=n;i--;)
+#define rrep2(i,n) for(ll i=n;i--;)
+#define rrep3(i,a,b) for(ll i=b;i-->(a);)
+#define rrep4(i,a,b,c) for(ll i=(a)+((b)-(a)-1)/(c)*(c);i>=(a);i-=c)
+#define rrep(...) overload4(__VA_ARGS__,rrep4,rrep3,rrep2,rrep1)(__VA_ARGS__)
+#define ALL(v) v.begin(), v.end()
+#define endl "\n"
+#define fi first
+#define se second
+#define popcount(bit) __builtin_popcount(bit)
+#define popcountll(bit) __builtin_popcountll(bit)
+#define pb push_back
+#define eb emplace_back
+#define sz(x) ((ll)(x).size())
+using namespace atcoder;
+using P = pair<int, int>;
+using PL = pair<long long, long long>;
+using Graph = vector<vector<int>>;
+typedef long long ll;
+template<class T> inline bool chmax(T& a, T b) { if (a < b) { a = b; return 1; } return 0; }
+template<class T> inline bool chmin(T& a, T b) { if (a > b) { a = b; return 1; } return 0; }
+inline void Yes(bool b = true) { cout << (b ? "Yes" : "No") << '\n'; }
+inline void YES(bool b = true) { cout << (b ? "YES" : "NO") << '\n'; }
+inline void OKNG(bool b = true) { cout << (b ? "OK" : "NG") << '\n'; }
+const int dx[4] = {1, 0, -1, 0};
+const int dy[4] = {0, 1, 0, -1};
+const int fx[8] = {0, 1, 1, 1, 0, -1, -1, -1};
+const int fy[8] = {1, 1, 0, -1, -1, -1, 0, 1};
+template <typename T>
+const auto INF = numeric_limits<T>::max()/2;
+
+int main() {
+  int n;
+  cin >> n;
+  dsu d(n);
+  vector<vector<int>> G(n);
+  vector<int> deg(n);
+  vector<int> A(n), B(n);
+  map<pair<int, int>, int> mp;
+  rep(i, n){
+    int a, b;
+    cin >> a >> b;
+    a--, b--;
+    G[a].pb(b);
+    G[b].pb(a);
+    A[i] = a, B[i] = b;
+    mp[{a, b}] = i;
+    mp[{b, a}] = i;
+    deg[a]++;
+    deg[b]++;
+  }
+  vector<int> namori(n, 1);
+  queue<int> que;
+  rep(i, n)if(deg[i] == 1){
+    que.push(i);
+    namori[i] = 0;
+  }
+  while (!que.empty()){
+    int u = que.front();
+    que.pop();
+    for(auto &v: G[u]){
+      if(namori[v] == 0)continue;
+      deg[v]--;
+      if(deg[v] <= 1){
+        namori[v] = 0;
+        que.push(v);
+      }
+    }
+  }
+  vector<int> id(n);
+  auto dfs = [&](auto &&self, int v, int u) -> void{
+    id[v] = id[u];
+    for(auto &p : G[v]){
+      if(p == u)continue;
+      self(self, p, v);
+    }
+  };
+  rep(i, n){
+    if(namori[i]){
+      id[i] = i;
+      for(auto &v : G[i]){
+        if(namori[v] == 1)continue;
+        dfs(dfs, v, i);
+      }
+    }
+  }
+  int q;
+  cin >> q;
+  rep(i, q){
+    int a, b;
+    cin >> a >> b;
+    a--, b--;
+    if(id[a] == id[b]){
+      cout << "Yes" << endl;
+    }else{
+      cout << "No" << endl;
+    }
+  }
+  
+}
