5d6cd53c58bd0bbc33f0d00eff84532dced988db
hayaten0415 <hayaten0415@gmail.com>
2022-10-02 00:28:26 +0900

    ABC265 E

---
+++ AtCoder/ABC/ABC265/E.cpp
@@ -0,0 +1,78 @@
+#include <bits/stdc++.h>
+#include <atcoder/all>
+using namespace std;
+#define REP(i,n) for(ll i=0;i<ll(n);i++)
+#define REPD(i,n) for(ll i=n-1;i>=0;i--)
+#define FOR(i,a,b) for(ll i=a;i<=ll(b);i++)
+#define FORD(i,a,b) for(ll i=a;i>=ll(b);i--)
+#define overload4(_1,_2,_3,_4,name,...) name
+#define overload3(_1,_2,_3,name,...) name
+#define rep1(n) for(ll i=0;i<n;++i)
+#define rep2(i,n) for(ll i=0;i<n;++i)
+#define rep3(i,a,b) for(ll i=a;i<b;++i)
+#define rep4(i,a,b,c) for(ll i=a;i<b;i+=c)
+#define rep(...) overload4(__VA_ARGS__,rep4,rep3,rep2,rep1)(__VA_ARGS__)
+#define rrep1(n) for(ll i=n;i--;)
+#define rrep2(i,n) for(ll i=n;i--;)
+#define rrep3(i,a,b) for(ll i=b;i-->(a);)
+#define rrep4(i,a,b,c) for(ll i=(a)+((b)-(a)-1)/(c)*(c);i>=(a);i-=c)
+#define rrep(...) overload4(__VA_ARGS__,rrep4,rrep3,rrep2,rrep1)(__VA_ARGS__)
+#define ALL(v) v.begin(), v.end()
+#define endl "\n"
+#define fi first
+#define se second
+#define popcount(bit) __builtin_popcount(bit)
+#define popcountll(bit) __builtin_popcountll(bit)
+#define pb push_back
+#define eb emplace_back
+#define sz(x) ((ll)(x).size())
+using namespace atcoder;
+using P = pair<int, int>;
+using PL = pair<long long, long long>;
+using Graph = vector<vector<int>>;
+typedef long long ll;
+template<class T> inline bool chmax(T& a, T b) { if (a < b) { a = b; return 1; } return 0; }
+template<class T> inline bool chmin(T& a, T b) { if (a > b) { a = b; return 1; } return 0; }
+inline void Yes(bool b = true) { cout << (b ? "Yes" : "No") << '\n'; }
+inline void YES(bool b = true) { cout << (b ? "YES" : "NO") << '\n'; }
+inline void OKNG(bool b = true) { cout << (b ? "OK" : "NG") << '\n'; }
+const int dx[4] = {1, 0, -1, 0};
+const int dy[4] = {0, 1, 0, -1};
+const int fx[8] = {0, 1, 1, 1, 0, -1, -1, -1};
+const int fy[8] = {1, 1, 0, -1, -1, -1, 0, 1};
+template <typename T>
+const auto INF = numeric_limits<T>::max()/2;
+using mint = modint998244353;
+
+int main() {
+  int n, m;
+  cin >> n >> m;
+  map<PL, mint> dp;
+  vector<ll> A(3), B(3);
+  rep(i, 3){
+    cin >> A[i] >> B[i];
+  }
+  set<pair<int,int>>S;
+	for(int i=0;i<m;i++){
+		int a,b;
+		cin >> a >> b;
+		S.insert({a,b});
+	}
+  dp[{0, 0}] = 1;
+  rep(i, n){
+    map<PL, mint> dp_tmp;
+    for(auto &[s, d] : dp){
+      auto[x, y] = s;
+      rep(k, 3){
+        ll nx = x + A[k], ny = y + B[k];
+        if(S.find({nx,ny})==S.end()){
+          dp_tmp[{nx, ny}] += d;
+        }
+      }
+    }
+    swap(dp_tmp, dp);
+  }
+  mint ans = 0;
+  for(auto &[x, value] : dp)ans += value;
+  cout << ans.val() << endl;
+}
