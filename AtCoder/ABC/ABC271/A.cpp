1d131d385a063acbaa79950c37f545f728e9f3f9
hayaten0415 <hayaten0415@gmail.com>
2022-10-01 23:51:38 +0900

    ABC271 A-E

---
+++ AtCoder/ABC/ABC271/A.cpp
@@ -0,0 +1,91 @@
+#include <bits/stdc++.h>
+#include <atcoder/all>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace std;
+using namespace __gnu_pbds;
+template <typename T>
+using ordered_set =
+    tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+struct __INIT{__INIT(){cin.tie(0);ios::sync_with_stdio(false);cout<<fixed<<setprecision(15);}} __init;
+#define REP(i,n) for(ll i=0;i<ll(n);i++)
+#define REPD(i,n) for(ll i=n-1;i>=0;i--)
+#define FOR(i,a,b) for(ll i=a;i<=ll(b);i++)
+#define FORD(i,a,b) for(ll i=a;i>=ll(b);i--)
+#define overload4(_1,_2,_3,_4,name,...) name
+#define overload3(_1,_2,_3,name,...) name
+#define rep1(n) for(ll i=0;i<n;++i)
+#define rep2(i,n) for(ll i=0;i<n;++i)
+#define rep3(i,a,b) for(ll i=a;i<b;++i)
+#define rep4(i,a,b,c) for(ll i=a;i<b;i+=c)
+#define rep(...) overload4(__VA_ARGS__,rep4,rep3,rep2,rep1)(__VA_ARGS__)
+#define rrep1(n) for(ll i=n;i--;)
+#define rrep2(i,n) for(ll i=n;i--;)
+#define rrep3(i,a,b) for(ll i=b;i-->(a);)
+#define rrep4(i,a,b,c) for(ll i=(a)+((b)-(a)-1)/(c)*(c);i>=(a);i-=c)
+#define rrep(...) overload4(__VA_ARGS__,rrep4,rrep3,rrep2,rrep1)(__VA_ARGS__)
+#define ALL(v) v.begin(), v.end()
+#define endl "\n"
+#define fi first
+#define se second
+#define popcount(bit) __builtin_popcount(bit)
+#define popcountll(bit) __builtin_popcountll(bit)
+#define pb push_back
+#define eb emplace_back
+#define sz(x) ((ll)(x).size())
+using namespace atcoder;
+using P = pair<int, int>;
+using PL = pair<long long, long long>;
+using Graph = vector<vector<int>>;
+typedef long long ll;
+template<class T> inline bool chmax(T& a, T b) { if (a < b) { a = b; return 1; } return 0; }
+template<class T> inline bool chmin(T& a, T b) { if (a > b) { a = b; return 1; } return 0; }
+inline void Yes(bool b = true) { cout << (b ? "Yes" : "No") << '\n'; }
+inline void YES(bool b = true) { cout << (b ? "YES" : "NO") << '\n'; }
+inline void OKNG(bool b = true) { cout << (b ? "OK" : "NG") << '\n'; }
+const int dx[4] = {1, 0, -1, 0};
+const int dy[4] = {0, 1, 0, -1};
+const int fx[8] = {0, 1, 1, 1, 0, -1, -1, -1};
+const int fy[8] = {1, 1, 0, -1, -1, -1, 0, 1};
+template <typename T>
+const auto INF = numeric_limits<T>::max()/2;
+
+/**
+* 10進数 v を base進数の文字列へ変換する。
+* @param [in] v    10進数の整数
+* @param [in] base 変換した 2~36 のN進数の値
+* @return base進数へ変換した文字列
+*/
+template<typename TypeInt>
+std::string Itoa(const TypeInt v, int base)
+{
+	static const char table[] = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	string ret;
+	static numeric_limits<TypeInt> t;
+	TypeInt n = v;
+	if (t.is_signed) {
+		if (v < 0) n *= -1;
+	}
+
+	while (n >= base) {
+		ret += table[n%base];
+		n /= base;
+	}
+	ret += table[n];
+	if (t.is_signed) {
+		if (v < 0 && base == 10) ret += '-';
+	}
+	// 文字列の順番を逆にする
+	std::reverse(ret.begin(), ret.end());
+
+	return ret;
+}
+
+
+int main() {
+  ll n;
+  cin >> n;
+  string ans = Itoa(n, 16);
+  if(ans.size() == 1)ans = "0" + ans;
+  cout << ans << endl;
+}
